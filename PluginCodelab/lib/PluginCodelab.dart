cbaffc3fcb9cac7cabd5d88ccf45c15e93d31a60
Aaron Clarke <aaclarke@google.com>
2020-06-23 21:37:44 -0700

    turned on analyze and format for the plugin

--- PluginCodelab/lib/PluginCodelab.dart
+++ PluginCodelab/lib/PluginCodelab.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class PluginCodelab {
-  static const MethodChannel _channel =
-      const MethodChannel('PluginCodelab');
+  static const MethodChannel _channel = const MethodChannel('PluginCodelab');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
