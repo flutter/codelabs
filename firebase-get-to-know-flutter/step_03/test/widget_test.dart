4311aee3825cb482629c5b35246bd8329468fe79
Brett Morgan <brett.morgan@gmail.com>
2020-09-20 18:41:03 +1000

    Make image an asset, add a test

---
+++ firebase-get-to-know-flutter/step_03/test/widget_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:gtk_flutter/main.dart';
+
+void main() {
+  testWidgets('Basic rendering', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(GTKApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('Firebase Meetup'), findsOneWidget);
+    expect(find.text('January 1st'), findsNothing);
+  });
+}
