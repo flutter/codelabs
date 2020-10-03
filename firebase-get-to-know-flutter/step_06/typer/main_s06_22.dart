3c2c5e28ac516f1854dec2ff50d0abdb0b835592
Brett Morgan <brett.morgan@gmail.com>
2020-10-03 22:01:20 +1000

    Clean up steps

--- firebase-get-to-know-flutter/step_06/typer/main_s06_22.dart
+++ firebase-get-to-know-flutter/step_06/typer/main_s06_22.dart
@@ -80,7 +80,7 @@ class HomePage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Header('Discussion'),
-                GuestBook(addMessage: (String message) => print(message)),
+                GuestBook(addMessage: (String message) async => print(message)),
               ],
             ),
           ),
