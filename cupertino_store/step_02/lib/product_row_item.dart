300a0d4b5850d7b4d45645c57c514b88aba7d630
Brett Morgan <brettmorgan@google.com>
2021-01-12 09:09:44 +1100

    Add cupertino_store codelab (#40)

---
+++ cupertino_store/step_02/lib/product_row_item.dart
@@ -0,0 +1,111 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+import 'model/app_state_model.dart';
+import 'model/product.dart';
+import 'styles.dart';
+
+class ProductRowItem extends StatelessWidget {
+  const ProductRowItem({
+    this.index,
+    this.product,
+    this.lastItem,
+  });
+
+  final Product product;
+  final int index;
+  final bool lastItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final row = SafeArea(
+      top: false,
+      bottom: false,
+      minimum: const EdgeInsets.only(
+        left: 16,
+        top: 8,
+        bottom: 8,
+        right: 8,
+      ),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Image.asset(
+              product.assetName,
+              package: product.assetPackage,
+              fit: BoxFit.cover,
+              width: 76,
+              height: 76,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    product.name,
+                    style: Styles.productRowItemName,
+                  ),
+                  const Padding(padding: EdgeInsets.only(top: 8)),
+                  Text(
+                    '\$${product.price}',
+                    style: Styles.productRowItemPrice,
+                  )
+                ],
+              ),
+            ),
+          ),
+          CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: () {
+              final model = Provider.of<AppStateModel>(context, listen: false);
+              model.addProductToCart(product.id);
+            },
+            child: const Icon(
+              CupertinoIcons.plus_circled,
+              semanticLabel: 'Add',
+            ),
+          ),
+        ],
+      ),
+    );
+
+    if (lastItem) {
+      return row;
+    }
+
+    return Column(
+      children: <Widget>[
+        row,
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 100,
+            right: 16,
+          ),
+          child: Container(
+            height: 1,
+            color: Styles.productRowDivider,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ cupertino_store/step_04/android/app/src/main/kotlin/com/example/cupertino_store/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cupertino_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
