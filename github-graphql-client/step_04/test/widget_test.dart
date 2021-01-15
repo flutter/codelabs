7ca640f6b9ac35eba679e74a2ff828166b697aa2
Brett Morgan <brett.morgan@gmail.com>
2021-01-15 13:26:01 +1100

    Rebuild github-graphql-client/step_04

--- github-graphql-client/step_04/test/widget_test.dart
+++ github-graphql-client/step_04/test/widget_test.dart
@@ -19,6 +19,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:github_graphql_client/main.dart';
@@ -29,7 +30,15 @@ void main() {
     await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('Github Login'), findsOneWidget);
-    expect(find.text('Not this text'), findsNothing);
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
