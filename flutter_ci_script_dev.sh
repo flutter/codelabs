74d0e6c85e877acaf59a81bbb6911c45a46624bc
Brett Morgan <brett.morgan@gmail.com>
2021-01-27 07:29:43 +1100

    Add plugin_codelab to beta and dev CI

--- flutter_ci_script_dev.sh
+++ flutter_ci_script_dev.sh
@@ -23,8 +23,7 @@ declare -ar CODELABS=(
   "firebase-get-to-know-flutter"
   "github-graphql-client"
   "google-maps-in-flutter"
-  # Plugin codelab's example project is unhappy with ubuntu-latest 
-  # "plugin_codelab"
+  "plugin_codelab"
   "startup_namer"
   # Tracking issue: https://github.com/flutter/flutter/issues/74209	
   # "testing_codelab"  
