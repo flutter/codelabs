106a424ccbc97a878fd6f0eb54309f612c75f440
“abd99” <abdullahzakir99@gmail.com>
2021-02-10 09:45:36 +0530

    Remove performance recording from fvrts oprtn tst

--- testing_codelab/step_07/integration_test/app_test.dart
+++ testing_codelab/step_07/integration_test/app_test.dart
@@ -33,38 +33,34 @@ void main() {
     testWidgets('Favorites operations test', (tester) async {
       await tester.pumpWidget(TestingApp());
 
-      await binding.traceAction(() async {
-        await binding.watchPerformance(() async {
-          final iconKeys = [
-            'icon_0',
-            'icon_1',
-            'icon_2',
-          ];
+      final iconKeys = [
+        'icon_0',
+        'icon_1',
+        'icon_2',
+      ];
 
-          for (var icon in iconKeys) {
-            await tester.tap(find.byKey(ValueKey(icon)));
-            await tester.pumpAndSettle();
+      for (var icon in iconKeys) {
+        await tester.tap(find.byKey(ValueKey(icon)));
+        await tester.pumpAndSettle();
 
-            expect(find.text('Added to favorites.'), findsOneWidget);
-          }
+        expect(find.text('Added to favorites.'), findsOneWidget);
+      }
 
-          await tester.tap(find.text('Favorites'));
-          await tester.pumpAndSettle();
+      await tester.tap(find.text('Favorites'));
+      await tester.pumpAndSettle();
 
-          final removeIconKeys = [
-            'remove_icon_0',
-            'remove_icon_1',
-            'remove_icon_2',
-          ];
+      final removeIconKeys = [
+        'remove_icon_0',
+        'remove_icon_1',
+        'remove_icon_2',
+      ];
 
-          for (final iconKey in removeIconKeys) {
-            await tester.tap(find.byKey(ValueKey(iconKey)));
-            await tester.pumpAndSettle();
+      for (final iconKey in removeIconKeys) {
+        await tester.tap(find.byKey(ValueKey(iconKey)));
+        await tester.pumpAndSettle();
 
-            expect(find.text('Removed from favorites.'), findsOneWidget);
-          }
-        }, reportKey: 'favorites_operations_summary');
-      }, reportKey: 'favorites_operations_timeline');
+        expect(find.text('Removed from favorites.'), findsOneWidget);
+      }
     });
   });
 }
