1684c31453f10279fd99acc65de2e1481b70c506
Matt Carroll <matthew-carroll@users.noreply.github.com>
2021-02-21 21:43:38 -0800

    Implemented new nested nav example.

--- cookbook/lib/examples/nested_nav.dart
+++ cookbook/lib/examples/nested_nav.dart
@@ -4,20 +4,37 @@
 
 import 'package:flutter/material.dart';
 
+const routeHome = '/';
+const routeSettings = '/settings';
+const routePrefixDeviceSetup = '/setup/';
+const routeDeviceSetupStart = '/setup/$routeDeviceSetupStartPage';
+const routeDeviceSetupStartPage = 'find_devices';
+const routeDeviceSetupSelectDevicePage = 'select_device';
+const routeDeviceSetupConnectingPage = 'connecting';
+const routeDeviceSetupFinishedPage = 'finished';
+
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.blue,
+        ),
+      ),
       onGenerateRoute: (settings) {
-        print('Generating route for ${settings.name}');
         late Widget page;
-        if (settings.name == '/') {
-          page = LandingScreen();
-        } else if (settings.name == '/account') {
-          page = AccountScreen();
-        } else if (settings.name!.startsWith('/setup:')) {
-          final subRoute = settings.name!.substring('/setup:'.length);
+        if (settings.name == routeHome) {
+          page = HomeScreen();
+        } else if (settings.name == routeSettings) {
+          page = SettingsScreen();
+        } else if (settings.name!.startsWith(routePrefixDeviceSetup)) {
+          final subRoute = settings.name!.substring(routePrefixDeviceSetup.length);
           page = SetupFlow(
-            initialSetupRoute: subRoute,
+            setupPageRoute: subRoute,
           );
         } else {
           throw Exception('Unknown route: ${settings.name}');
@@ -43,10 +60,10 @@ class SetupFlow extends StatefulWidget {
 
   const SetupFlow({
     Key? key,
-    this.initialSetupRoute = '/install',
+    required this.setupPageRoute,
   }) : super(key: key);
 
-  final String initialSetupRoute;
+  final String setupPageRoute;
 
   @override
   SetupFlowState createState() => SetupFlowState();
@@ -55,123 +72,285 @@ class SetupFlow extends StatefulWidget {
 class SetupFlowState extends State<SetupFlow> {
   final _navigatorKey = GlobalKey<NavigatorState>();
 
-  void exitSetup() {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _onDiscoveryComplete() {
+    _navigatorKey.currentState!.pushNamed(routeDeviceSetupSelectDevicePage);
+  }
+
+  void _onDeviceSelected(String deviceId) {
+    _navigatorKey.currentState!.pushNamed(routeDeviceSetupConnectingPage);
+  }
+
+  void _onConnectionEstablished() {
+    _navigatorKey.currentState!.pushNamed(routeDeviceSetupFinishedPage);
+  }
+
+  Future<void> _onExitPressed() async {
+    final isConfirmed = await _isExitDesired();
+
+    if (isConfirmed && mounted) {
+      _exitSetup();
+    }
+  }
+
+  Future<bool> _isExitDesired() async {
+    return await showDialog<bool>(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('Are you sure?'),
+                content: Text('If you exit device setup, your progress will be lost.'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(true);
+                    },
+                    child: Text('Leave'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
+                    },
+                    child: Text('Stay'),
+                  ),
+                ],
+              );
+            }) ??
+        false;
+  }
+
+  void _exitSetup() {
     Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () async =>
-          await _navigatorKey.currentState?.maybePop() ?? true,
-      child: Navigator(
-        key: _navigatorKey,
-        initialRoute: widget.initialSetupRoute,
-        onGenerateRoute: (settings) {
-          print('Setup: generating route: ${settings.name}');
-          late Widget page;
-          switch (settings.name) {
-            case 'install':
-              page = InstallPage();
-              break;
-            case 'connecting':
-              page = ConnectingPage();
-              break;
-            case 'review':
-              page = ReviewPage();
-              break;
-          }
-
-          return MaterialPageRoute<dynamic>(
-            builder: (context) {
-              return page;
-            },
-            settings: settings,
-          );
-        },
+      onWillPop: _isExitDesired,
+      child: Scaffold(
+        appBar: _buildFlowAppBar(),
+        body: Navigator(
+          key: _navigatorKey,
+          initialRoute: widget.setupPageRoute,
+          onGenerateRoute: _onGenerateRoute,
+        ),
       ),
+    );
+  }
+
+  Route _onGenerateRoute(RouteSettings settings) {
+    late Widget page;
+    switch (settings.name) {
+      case routeDeviceSetupStartPage:
+        page = WaitingPage(
+          message: 'Searching for nearby bulb...',
+          onWaitComplete: _onDiscoveryComplete,
+        );
+        break;
+      case routeDeviceSetupSelectDevicePage:
+        page = SelectDevicePage(
+          onDeviceSelected: _onDeviceSelected,
+        );
+        break;
+      case routeDeviceSetupConnectingPage:
+        page = WaitingPage(
+          message: 'Connecting...',
+          onWaitComplete: _onConnectionEstablished,
+        );
+        break;
+      case routeDeviceSetupFinishedPage:
+        page = FinishedPage(
+          onFinishPressed: _exitSetup,
+        );
+        break;
+    }
+
+    return MaterialPageRoute<dynamic>(
+      builder: (context) {
+        return page;
+      },
+      settings: settings,
+    );
+  }
+
+  PreferredSizeWidget _buildFlowAppBar() {
+    return AppBar(
+      leading: IconButton(
+        onPressed: _onExitPressed,
+        icon: Icon(Icons.chevron_left),
+      ),
+      title: Text('Bulb Setup'),
     );
   }
 }
 
-class InstallPage extends StatelessWidget {
+class SelectDevicePage extends StatelessWidget {
+  const SelectDevicePage({
+    Key? key,
+    required this.onDeviceSelected,
+  }) : super(key: key);
+
+  final void Function(String deviceId) onDeviceSelected;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('Install your device'),
-            FlatButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('connecting');
-              },
-              child: Text('Connect to the server'),
-            ),
-            FlatButton(
-              onPressed: () {
-                SetupFlow.of(context).exitSetup();
-              },
-              child: Text('Exit Setup'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Select a nearby device:',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(height: 24),
+              SizedBox(
+                width: double.infinity,
+                height: 54,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateColor.resolveWith((states) {
+                      return const Color(0xFF222222);
+                    }),
+                  ),
+                  onPressed: () {
+                    onDeviceSelected('22n483nk5834');
+                  },
+                  child: Text(
+                    'Bulb 22n483nk5834',
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class ConnectingPage extends StatelessWidget {
+class WaitingPage extends StatefulWidget {
+  const WaitingPage({
+    Key? key,
+    required this.message,
+    required this.onWaitComplete,
+  }) : super(key: key);
+
+  final String message;
+  final VoidCallback onWaitComplete;
+
+  @override
+  _WaitingPageState createState() => _WaitingPageState();
+}
+
+class _WaitingPageState extends State<WaitingPage> {
+  @override
+  void initState() {
+    super.initState();
+    _startWaiting();
+  }
+
+  Future<void> _startWaiting() async {
+    await Future<dynamic>.delayed(const Duration(seconds: 3));
+
+    if (mounted) {
+      widget.onWaitComplete();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('Connecting...'),
-            FlatButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('review');
-              },
-              child: Text('Review device setup'),
-            ),
-            FlatButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Back to install'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircularProgressIndicator(),
+              SizedBox(height: 32),
+              Text(widget.message),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class ReviewPage extends StatelessWidget {
+class FinishedPage extends StatelessWidget {
+  const FinishedPage({
+    Key? key,
+    required this.onFinishPressed,
+  }) : super(key: key);
+
+  final VoidCallback onFinishPressed;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('Review the setup.'),
-            FlatButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Reconnect'),
-            ),
-            FlatButton(
-              onPressed: () {
-                SetupFlow.of(context).exitSetup();
-              },
-              child: Text('Exit Setup'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: 250,
+                height: 250,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: const Color(0xFF222222),
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.lightbulb,
+                    size: 175,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 32),
+              Text(
+                'Bulb added!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 32),
+              ElevatedButton(
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.resolveWith((states) {
+                    return const EdgeInsets.symmetric(horizontal: 24, vertical: 12);
+                  }),
+                  backgroundColor: MaterialStateColor.resolveWith((states) {
+                    return const Color(0xFF222222);
+                  }),
+                  shape: MaterialStateProperty.resolveWith((states) {
+                    return StadiumBorder();
+                  }),
+                ),
+                onPressed: onFinishPressed,
+                child: Text(
+                  'Finish',
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -179,94 +358,104 @@ class ReviewPage extends StatelessWidget {
 }
 
 @immutable
-class LandingScreen extends StatelessWidget {
-  const LandingScreen({
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: _buildAppBar(context),
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Welcome!',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/setup:install');
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blueAccent,
-                shape: const StadiumBorder(),
-                textStyle: const TextStyle(
-                  fontSize: 22.0,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: 250,
+                height: 250,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: const Color(0xFF222222),
                 ),
-                minimumSize: const Size(48.0, 48.0),
-              ),
-              child: const Text('Setup your device'),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/account');
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blueAccent,
-                shape: const StadiumBorder(),
-                textStyle: const TextStyle(
-                  fontSize: 22.0,
+                child: Center(
+                  child: Icon(
+                    Icons.lightbulb,
+                    size: 175,
+                    color: Theme.of(context).scaffoldBackgroundColor,
+                  ),
                 ),
-                minimumSize: const Size(48.0, 48.0),
               ),
-              child: const Text('My Account'),
-            ),
-          ],
+              SizedBox(height: 32),
+              Text(
+                'Add your first bulb',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed(routeDeviceSetupStart);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar(BuildContext context) {
+    return AppBar(
+      title: Text('Welcome'),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.settings),
+          onPressed: () {
+            Navigator.pushNamed(context, routeSettings);
+          },
+        ),
+      ],
     );
   }
 }
 
-class AccountScreen extends StatelessWidget {
-  const AccountScreen({
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      appBar: _buildAppBar(),
+      body: SingleChildScrollView(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Account Screen',
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blueAccent,
-                shape: const StadiumBorder(),
-                textStyle: const TextStyle(
-                  fontSize: 22.0,
-                ),
-                minimumSize: const Size(48.0, 48.0),
+          children: List.generate(8, (index) {
+            return Container(
+              width: double.infinity,
+              height: 54,
+              margin: const EdgeInsets.only(left: 16, right: 16, top: 16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: const Color(0xFF222222),
               ),
-              child: const Text('Back to Home'),
-            ),
-          ],
+            );
+          }),
         ),
       ),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      title: Text('Settings'),
     );
   }
 }
