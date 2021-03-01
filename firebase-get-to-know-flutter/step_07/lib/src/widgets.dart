8700c649aaafd9cbfcb98b1c115da49418a31977
Brett Morgan <brett.morgan@gmail.com>
2021-03-01 16:47:04 +1100

    Step 7

--- firebase-get-to-know-flutter/step_07/lib/src/widgets.dart
+++ firebase-get-to-know-flutter/step_07/lib/src/widgets.dart
@@ -49,15 +49,14 @@ class IconAndDetail extends StatelessWidget {
 }
 
 class StyledButton extends StatelessWidget {
-  const StyledButton({@required this.child, @required this.onPressed});
+  const StyledButton({required this.child, required this.onPressed});
   final Widget child;
   final void Function() onPressed;
 
   @override
   Widget build(BuildContext context) => OutlinedButton(
         style: OutlinedButton.styleFrom(
-          side: BorderSide(color: Colors.deepPurple),
-        ),
+            side: BorderSide(color: Colors.deepPurple)),
         onPressed: onPressed,
         child: child,
       );
