66f174550e9d712ea3f170f51625e355de83234a
Brett Morgan <brett.morgan@gmail.com>
2021-03-09 14:35:26 +1100

    first pass

---
+++ star_counter/step_04/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(StarCounterApp());
+}
+
+class StarCounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      routes: {
+        '/': (context) => HomePage(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _repositoryName = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 400),
+          child: Card(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'GitHub Star Counter',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter a GitHub repository',
+                      hintText: 'flutter/flutter',
+                    ),
+                    onSubmitted: (text) {
+                      setState(() {
+                        _repositoryName = text;
+                      });
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 32.0),
+                    child: Text(
+                      _repositoryName,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
