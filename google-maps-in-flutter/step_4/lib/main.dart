423e140f99a9332680ed2c1bc70b62107ec611ba
Brett Morgan <brett.morgan@gmail.com>
2021-03-22 15:39:14 +1100

    Update

--- google-maps-in-flutter/step_4/lib/main.dart
+++ google-maps-in-flutter/step_4/lib/main.dart
@@ -17,9 +17,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
