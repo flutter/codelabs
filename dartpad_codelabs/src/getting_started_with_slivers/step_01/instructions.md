842ce57726e01cf6d719938113c818354db4a096
Kate Lovett <katelovett@google.com>
2021-04-19 16:22:59 -0500

    Init

---
+++ dartpad_codelabs/src/getting_started_with_slivers/step_01/instructions.md
@@ -0,0 +1,19 @@
+# Introduction
+
+Welcome to the introductory sliver codelab for Flutter!
+In this workshop, you will learn how to efficiently build
+scrolling widgets and use slivers directly to create a rich
+scrolling experience for users.
+
+What is a sliver? A sliver is a portion of scrollable area,
+which means anything that scrolls in Flutter is a sliver.
+
+This codelab is best for folks already familiar with building simple
+Flutter applications. If this is your first time using Flutter,
+check out [Writing Your First Flutter App](https://flutter.dev/docs/get-started/codelab)
+to get started using Flutter.
+
+
+
+
+
---
+++ dartpad_codelabs/src/getting_started_with_slivers/step_01/snippet.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import 'server.dart';
+
+void main() {
+  runApp(HorizonsApp());
+}
+
+class HorizonsApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // This is the theme of your application.
+      theme: ThemeData.dark(),
+      // Scrolling in Flutter behaves differently depending on the ScrollBehavior.
+      // By default, ScrollBehavior changes depending on the current platform.
+      // For the purposes of this scrolling codelab, we're using a custom
+      // ScrollBehavior so that the experience is the same for everyone
+      // regardless of the platform they are using.
+      scrollBehavior: const ConstantScrollBehavior(),
+      title: 'Horizons Weather',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Horizons'),
+          backgroundColor: Colors.teal[800],
+        ),
+        body: WeeklyForecastList(),
+      )
+    );
+  }
+}
+
+class WeeklyForecastList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final DateTime currentDate = DateTime.now();
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    final List<DailyForecast> forecasts = Server.getDailyForecastList();
+    
+    return SingleChildScrollView(
+      child: Column(
+        children: forecasts.map((DailyForecast forecast) {
+          return Card(
+            child: ListTile(
+              title: Text(
+                forecast.getWeekday(currentDate.weekday),
+                style: textTheme.headline5,
+              ),
+              subtitle: Text(forecast.description),
+              leading: Text(
+                forecast.getDate(currentDate.day).toString(),
+                style: textTheme.headline4,
+              ),
+              trailing: Text(
+                '${forecast.highTemp}° H / ${forecast.lowTemp}° L',
+                style: textTheme.subtitle2,
+              ),
+            ),
+          );
+        }).toList(),
+      )  
+    );
+  }
+}
