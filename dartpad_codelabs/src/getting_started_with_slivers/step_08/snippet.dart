9ba6104dfb8a3afc13666e43adb7183027a6f6c0
John Ryan <ryjohn@google.com>
2021-04-28 15:08:56 -0700

    Setup DartPad workshop hosting (#101)
    * Use dartpad.dev links and dartpad-workshops-io2021 project
    
    * update urls to load assets from dartpad-workshops-io2021.web.app
    
    * add links for Slivers and InheritedWidget codelabs

--- dartpad_codelabs/src/getting_started_with_slivers/step_08/snippet.dart
+++ dartpad_codelabs/src/getting_started_with_slivers/step_08/snippet.dart
@@ -140,7 +140,7 @@ class WeeklyForecastList extends StatelessWidget {
 // -----------------------------------------------------------------------------
 // Below this line are helper classes and data.
 
-const String baseAssetURL = 'http://localhost:8000/example/codelabs/slivers/';
+const String baseAssetURL = 'https://dartpad-workshops-io2021.web.app/getting_started_with_slivers/';
 
 const Map<int, DailyForecast> _kDummyData = {
   0 : DailyForecast(
