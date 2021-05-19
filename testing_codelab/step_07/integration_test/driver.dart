922cfd3f9fd43cec3262e63c085a7d90ef5fdd32
Brett Morgan <brett.morgan@gmail.com>
2021-05-20 08:03:22 +1000

    Updates from @abd99

--- testing_codelab/step_07/integration_test/driver.dart
+++ testing_codelab/step_07/integration_test/driver.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Fix for integration_test in https://github.com/flutter/flutter/pull/75074
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:integration_test/integration_test_driver.dart';
 
 Future<void> main() => integrationDriver();
