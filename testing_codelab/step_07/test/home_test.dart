36644094b3a7c1c538e62dd569b80f184b9b4682
Brett Morgan <brettmorgan@google.com>
2021-05-24 14:15:42 +1000

    testing_codelab null safe migration + desktop runners (#85)

--- testing_codelab/step_07/test/home_test.dart
+++ testing_codelab/step_07/test/home_test.dart
@@ -19,38 +19,27 @@ void main() {
   group('Home Page Widget Tests', () {
     testWidgets('Testing if ListView shows up', (tester) async {
       await tester.pumpWidget(createHomeScreen());
-
       expect(find.byType(ListView), findsOneWidget);
     });
 
     testWidgets('Testing Scrolling', (tester) async {
       await tester.pumpWidget(createHomeScreen());
-
       expect(find.text('Item 0'), findsOneWidget);
-
       await tester.fling(find.byType(ListView), Offset(0, -200), 3000);
       await tester.pumpAndSettle();
-
       expect(find.text('Item 0'), findsNothing);
     });
 
     testWidgets('Testing IconButtons', (tester) async {
       await tester.pumpWidget(createHomeScreen());
-
       expect(find.byIcon(Icons.favorite), findsNothing);
-
       await tester.tap(find.byIcon(Icons.favorite_border).first);
       await tester.pumpAndSettle(Duration(seconds: 1));
-
       expect(find.text('Added to favorites.'), findsOneWidget);
-
       expect(find.byIcon(Icons.favorite), findsWidgets);
-
       await tester.tap(find.byIcon(Icons.favorite).first);
       await tester.pumpAndSettle(Duration(seconds: 1));
-
       expect(find.text('Removed from favorites.'), findsOneWidget);
-
       expect(find.byIcon(Icons.favorite), findsNothing);
     });
   });
--- testing_codelab/step_07/test_driver/app_test.dart
+++ testing_codelab/step_07/test_driver/app_test.dart
@@ -2,12 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Fix for flutter_driver in https://github.com/flutter/flutter/pull/74856
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Testing App Performance Tests', () {
-    FlutterDriver driver;
+    FlutterDriver? driver;
 
     setUpAll(() async {
       driver = await FlutterDriver.connect();
@@ -15,24 +17,24 @@ void main() {
 
     tearDownAll(() async {
       if (driver != null) {
-        await driver.close();
+        await driver?.close();
       }
     });
 
     test('Scrolling test', () async {
       final listFinder = find.byType('ListView');
 
-      final scrollingTimeline = await driver.traceAction(() async {
-        await driver.scroll(listFinder, 0, -7000, Duration(seconds: 1));
-        await driver.scroll(listFinder, 0, 7000, Duration(seconds: 1));
+      final scrollingTimeline = await driver?.traceAction(() async {
+        await driver?.scroll(listFinder, 0, -7000, Duration(seconds: 1));
+        await driver?.scroll(listFinder, 0, 7000, Duration(seconds: 1));
       });
 
-      final scrollingSummary = TimelineSummary.summarize(scrollingTimeline);
+      final scrollingSummary = TimelineSummary.summarize(scrollingTimeline!);
       await scrollingSummary.writeTimelineToFile('scrolling', pretty: true);
     });
 
     test('Favorites operations test', () async {
-      final operationsTimeline = await driver.traceAction(() async {
+      final operationsTimeline = await driver?.traceAction(() async {
         final iconKeys = [
           'icon_0',
           'icon_1',
@@ -40,11 +42,11 @@ void main() {
         ];
 
         for (var icon in iconKeys) {
-          await driver.tap(find.byValueKey(icon));
-          await driver.waitFor(find.text('Added to favorites.'));
+          await driver?.tap(find.byValueKey(icon));
+          await driver?.waitFor(find.text('Added to favorites.'));
         }
 
-        await driver.tap(find.text('Favorites'));
+        await driver?.tap(find.text('Favorites'));
 
         final removeIconKeys = [
           'remove_icon_0',
@@ -53,12 +55,12 @@ void main() {
         ];
 
         for (final iconKey in removeIconKeys) {
-          await driver.tap(find.byValueKey(iconKey));
-          await driver.waitFor(find.text('Removed from favorites.'));
+          await driver?.tap(find.byValueKey(iconKey));
+          await driver?.waitFor(find.text('Removed from favorites.'));
         }
       });
 
-      final operationsSummary = TimelineSummary.summarize(operationsTimeline);
+      final operationsSummary = TimelineSummary.summarize(operationsTimeline!);
       await operationsSummary.writeTimelineToFile('favorites_operations',
           pretty: true);
     });
