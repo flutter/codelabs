73fa75a7e17d4c5204c876ebee3c1d6884e2617e
David Bailey <davidbailey00@outlook.com>
2021-06-19 12:20:35 +0100

    startup_namer: add tooltip to saved suggestions button

--- startup_namer/step7_navigate_route/lib/main.dart
+++ startup_namer/step7_navigate_route/lib/main.dart
@@ -77,7 +77,11 @@ class _RandomWordsState extends State<RandomWords> {
       appBar: AppBar(
         title: const Text('Startup Name Generator'),
         actions: [
-          IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
+          ),
         ],
       ),
       body: _buildSuggestions(),
