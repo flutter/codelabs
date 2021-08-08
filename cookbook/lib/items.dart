2a84e34a406756b99751e16479c3ab88a1ccd6f3
Brett Morgan <brett.morgan@gmail.com>
2021-08-08 18:14:56 +1000

    Add runners, fix desktop issue
    Partial fix for https://github.com/flutter/flutter/issues/87892

--- cookbook/lib/items.dart
+++ cookbook/lib/items.dart
@@ -2,22 +2,25 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_cookbook/examples/cupertino_download_button.dart';
 import 'package:flutter_cookbook/examples/drag_and_drop.dart';
 import 'package:flutter_cookbook/examples/expandable_fab.dart';
 import 'package:flutter_cookbook/examples/gradient_bubbles.dart';
 import 'package:flutter_cookbook/examples/instagram_filter.dart';
-// import 'package:flutter_cookbook/examples/nested_nav.dart';
 import 'package:flutter_cookbook/examples/parallax.dart';
 import 'package:flutter_cookbook/examples/staggered_animation.dart';
 import 'package:flutter_cookbook/examples/typing_indicator.dart';
-// import 'package:flutter_cookbook/examples/ui_loading_animation.dart';
+import 'package:flutter_cookbook/examples/ui_loading_animation.dart';
 
 final items = <CookbookItem>[
-  CookbookItem('Parallax Scrolling', 'Scrolling', Recommendation.yes,
-      (_) => const ExampleParallax()),
-  // CookbookItem('Nested Navigation',               'Progress',   Recommendation.yes, (_) => const OnboardingFlow()),
+  CookbookItem(
+    'Parallax Scrolling',
+    'Scrolling',
+    Recommendation.yes,
+    (_) => const Scaffold(
+        body: Center(child: ExampleParallax()), backgroundColor: darkBlue),
+  ),
   CookbookItem('Gradient Bubbles', 'Transition', Recommendation.yes,
       (_) => const ExampleGradientBubbles()),
   CookbookItem('Download Button', 'Button', Recommendation.yes,
@@ -32,30 +35,17 @@ final items = <CookbookItem>[
       (_) => const ExampleStaggeredAnimations()),
   CookbookItem('Is Typing Animation', 'Animation', Recommendation.yes,
       (_) => const ExampleIsTyping()),
-  // CookbookItem('Animated Accordion',              'UI Pattern', Recommendation.yes),
-  // CookbookItem('Wallet Cards',                    'UI Pattern', Recommendation.maybe),
-  // CookbookItem('Accelerometer Example',           'Utility',    Recommendation.maybe),
-  // CookbookItem('Custom Painter / Equalizer',      'Utility',    Recommendation.maybe),
-  // CookbookItem('UI Loading Animations',           'Utility',    Recommendation.maybe, (_) => const ExampleUiLoadingAnimation()),
-  // CookbookItem('Perspective Carousel',            'Carousel',   Recommendation.maybe),
-  // CookbookItem('Swipe to Do X',                   'Animation',  Recommendation.maybe),
-  // CookbookItem('Gooey Swipe',                     'Animation',  Recommendation.maybe),
-  // CookbookItem('Vertical Horizontal Cards',       'Carousel',   Recommendation.maybe),
-  // CookbookItem('Progress Ring',                   'Animation',  Recommendation.maybe),
-  // @formatter:on
+  CookbookItem('UI Loading Animations', 'Utility', Recommendation.maybe,
+      (_) => const ExampleUiLoadingAnimation()),
 ];
 
 class CookbookItem {
-  CookbookItem(this.name, this.type, this.recommendation, [this.builder]);
+  CookbookItem(this.name, this.type, this.recommendation, this.builder);
 
   final String name;
   final String type;
   final Recommendation recommendation;
-  final WidgetBuilder? builder;
+  final WidgetBuilder builder;
 }
 
-enum Recommendation {
-  yes,
-  maybe,
-  no,
-}
+enum Recommendation { yes, maybe, no }
