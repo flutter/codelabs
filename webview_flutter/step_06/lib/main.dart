91ff01bab13fc0caee217f981a78327aefa53c7c
Jan-Derk de Vries <47666699+JDDV@users.noreply.github.com>
2021-10-28 13:23:23 +0200

    Update main.dart

--- webview_flutter/step_06/lib/main.dart
+++ webview_flutter/step_06/lib/main.dart
@@ -3,33 +3,12 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 void main() => runApp(MaterialApp(home: WebViewExample()));
 
-const String kNavigationExamplePage = '''
-<!DOCTYPE html>
-<html>
-  <head><title>Navigation Delegate Example</title></head>
-  <body>
-    <p>
-      The navigation delegate is set to block navigation to the youtube website.
-    </p>
-    <ul>
-      <ul><a href="https://www.youtube.com/">https://www.youtube.com/</a></ul>
-      <ul><a href="https://www.google.com/">https://www.google.com/</a></ul>
-    </ul>
-  </body>
-</html>
-''';
-
 class WebViewExample extends StatefulWidget {
   @override
   WebViewExampleState createState() => WebViewExampleState();
 }
 
 class WebViewExampleState extends State<WebViewExample> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return WebView(
