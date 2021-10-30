ece2a29fb176d436ea310df150a93ada0d823c79
florissmit1 <47666575+florissmit1@users.noreply.github.com>
2021-10-30 15:55:30 +0200

    [webview_flutter] Added step 06: Working with the WebViewController (#206)

---
+++ webview_flutter/step_06/test/widget_test.dart
@@ -0,0 +1,16 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:step_06/main.dart';
+
+void main() {
+  testWidgets('Do nothing test', (tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(WebViewExample());
+  });
+}
