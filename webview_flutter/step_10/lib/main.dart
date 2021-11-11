08c5108e76917345f50e096615f0366fcba3e2f9
Jan-Derk de Vries <janderk@baseflow.com>
2021-11-11 22:08:40 +0100

    removed used deprecated method

--- webview_flutter/step_10/lib/main.dart
+++ webview_flutter/step_10/lib/main.dart
@@ -85,7 +85,7 @@ class NavigationControls extends StatelessWidget {
                         await controller.goBack();
                       } else {
                         // ignore: deprecated_member_use
-                        Scaffold.of(context).showSnackBar(
+                        ScaffoldMessenger.of(context).showSnackBar(
                           const SnackBar(content: Text("No back history item")),
                         );
                         return;
@@ -100,8 +100,7 @@ class NavigationControls extends StatelessWidget {
                       if (await controller!.canGoForward()) {
                         await controller.goForward();
                       } else {
-                        // ignore: deprecated_member_use
-                        Scaffold.of(context).showSnackBar(
+                        ScaffoldMessenger.of(context).showSnackBar(
                           const SnackBar(
                               content: Text("No forward history item")),
                         );
