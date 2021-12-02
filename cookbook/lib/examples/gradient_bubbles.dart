73b1e933e2e4047245a56da8e63e3dd05c963ee1
Brett Morgan <brett.morgan@gmail.com>
2021-12-02 15:06:25 +1000

    CLeanup codelabs for Flutter 2.8.0 beta

--- cookbook/lib/examples/gradient_bubbles.dart
+++ cookbook/lib/examples/gradient_bubbles.dart
@@ -5,10 +5,7 @@
 import 'dart:math';
 import 'dart:ui' as ui;
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_cookbook/app.dart';
 
 void main() {
