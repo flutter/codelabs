235c6086381bbfd8c7d6a71e60aa81670e825d8f
Brett Morgan <brett.morgan@gmail.com>
2021-12-05 15:08:31 +1000

    `startup_namer`: Update for Flutter 2.8.0 beta

--- startup_namer/step2_use_package/lib/main.dart
+++ startup_namer/step2_use_package/lib/main.dart
@@ -5,9 +5,13 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
