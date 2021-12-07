e851b4261ce343fe9ab38f9dd0d850386b622158
Brett Morgan <brettmorgan@google.com>
2021-12-08 09:45:12 +1100

    `webview_flutter`: Replace `print` with SnackBar (#246)

--- webview_flutter/step_10/lib/main.dart
+++ webview_flutter/step_10/lib/main.dart
@@ -1,196 +1,41 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() => runApp(const MaterialApp(home: WebViewExample()));
+import 'src/menu.dart';
+import 'src/navigation_controls.dart';
+import 'src/web_view_stack.dart';
 
-class WebViewExample extends StatefulWidget {
-  const WebViewExample({Key? key}) : super(key: key);
-
-  @override
-  WebViewExampleState createState() => WebViewExampleState();
+void main() {
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
 }
 
-class WebViewExampleState extends State<WebViewExample> {
-  final _controller = Completer<WebViewController>();
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({Key? key}) : super(key: key);
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  final controller = Completer<WebViewController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter WebView example'),
+        title: const Text('Flutter WebView'),
         actions: [
-          NavigationControls(_controller.future),
-          Menu(_controller.future),
+          NavigationControls(controller: controller),
+          Menu(controller: controller),
         ],
       ),
-      body: Builder(builder: (context) {
-        return WebView(
-          initialUrl: 'https://flutter.dev',
-          onWebViewCreated: (webViewController) {
-            _controller.complete(webViewController);
-          },
-          onPageStarted: (url) {
-            print('Page started loading: $url');
-          },
-          onProgress: (progress) {
-            print('WebView is loading (progress : $progress%)');
-          },
-          onPageFinished: (url) {
-            print('Page finished loading: $url');
-          },
-          navigationDelegate: (navigation) {
-            if (Uri.parse(navigation.url).host.contains('youtube.com')) {
-              print('blocking navigation to $navigation}');
-              return NavigationDecision.prevent;
-            }
-            print('allowing navigation to $navigation');
-            return NavigationDecision.navigate;
-          },
-          javascriptMode: JavascriptMode.unrestricted,
-          javascriptChannels: _createJavascriptChannels(context),
-        );
-      }),
-    );
-  }
-
-  Set<JavascriptChannel> _createJavascriptChannels(BuildContext context) {
-    return {
-      JavascriptChannel(
-        name: 'SnackBar',
-        onMessageReceived: (message) {
-          ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: Text(message.message)));
-        },
-      ),
-    };
-  }
-}
-
-class NavigationControls extends StatelessWidget {
-  const NavigationControls(this._webViewControllerFuture, {Key? key})
-      : super(key: key);
-
-  final Future<WebViewController> _webViewControllerFuture;
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<WebViewController>(
-      future: _webViewControllerFuture,
-      builder: (context, snapshot) {
-        final webViewReady = snapshot.connectionState == ConnectionState.done;
-        final WebViewController? controller = snapshot.data;
-        if (!webViewReady || controller == null) {
-          return Row(
-            children: const <Widget>[
-              Icon(Icons.arrow_back_ios),
-              Icon(Icons.arrow_forward_ios),
-              Icon(Icons.replay),
-            ],
-          );
-        }
-
-        return Row(
-          children: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.arrow_back_ios),
-              onPressed: () async {
-                if (await controller.canGoBack()) {
-                  await controller.goBack();
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('No back history item')),
-                  );
-                  return;
-                }
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.arrow_forward_ios),
-              onPressed: () async {
-                if (await controller.canGoForward()) {
-                  await controller.goForward();
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('No forward history item')),
-                  );
-                  return;
-                }
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.replay),
-              onPressed: () {
-                controller.reload();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
-
-enum _MenuOptions {
-  navigationDelegate,
-  userAgent,
-  javascriptChannel,
-}
-
-class Menu extends StatelessWidget {
-  const Menu(this.controller, {Key? key}) : super(key: key);
-
-  final Future<WebViewController> controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<WebViewController>(
-      future: controller,
-      builder: (context, controller) {
-        return PopupMenuButton<_MenuOptions>(
-          onSelected: (value) async {
-            switch (value) {
-              case _MenuOptions.navigationDelegate:
-                controller.data!.loadUrl('https://m.youtube.com');
-                break;
-              case _MenuOptions.userAgent:
-                final userAgent = await controller.data!
-                    .runJavascriptReturningResult('navigator.userAgent');
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  content: Text(userAgent),
-                ));
-                break;
-              case _MenuOptions.javascriptChannel:
-                await controller.data!.runJavascript('''
-var req = new XMLHttpRequest();
-req.open('GET', "https://api.ipify.org/?format=json");
-req.onload = function() {
-  if (req.status == 200) {
-    SnackBar.postMessage(req.responseText);
-  } else {
-    SnackBar.postMessage("Error: " + req.status);
-  }
-}
-req.send();''');
-                break;
-            }
-          },
-          itemBuilder: (context) => [
-            const PopupMenuItem<_MenuOptions>(
-              value: _MenuOptions.navigationDelegate,
-              child: Text('Navigation Delegate Example'),
-            ),
-            const PopupMenuItem<_MenuOptions>(
-              value: _MenuOptions.userAgent,
-              child: Text('Show user-agent'),
-            ),
-            const PopupMenuItem<_MenuOptions>(
-              value: _MenuOptions.javascriptChannel,
-              child: Text('JavaScript Channel Example'),
-            ),
-          ],
-        );
-      },
+      body: WebViewStack(controller: controller),
     );
   }
 }
