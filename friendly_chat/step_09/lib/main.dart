7b745efb958eb0b84da59f18b78c4a282465b46e
Brett Morgan <brettmorgan@google.com>
2021-12-16 07:51:05 +1100

    Re-enable beta  (#254)

--- friendly_chat/step_09/lib/main.dart
+++ friendly_chat/step_09/lib/main.dart
@@ -15,7 +15,6 @@ void main() {
 final ThemeData kIOSTheme = ThemeData(
   primarySwatch: Colors.orange,
   primaryColor: Colors.grey[100],
-  primaryColorBrightness: Brightness.light,
 );
 
 final ThemeData kDefaultTheme = ThemeData(
