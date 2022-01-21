ee38271dd789feda1706764dc8f5756c163e4d5f
Miguel Beltran <m@beltran.work>
2022-01-21 16:02:08 +0100

    changes in window_to_front

--- github-client/window_to_front/macos/Classes/WindowToFrontPlugin.swift
+++ github-client/window_to_front/macos/Classes/WindowToFrontPlugin.swift
@@ -1,3 +1,17 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import Cocoa
 import FlutterMacOS
 
@@ -10,13 +24,9 @@ public class WindowToFrontPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    // Add from here
     case "activate":
       NSApplication.shared.activate(ignoringOtherApps: true)
       result(nil)
-    // to here.
-    // Delete the getPlatformVersion case,
-    // as we won’t be using it.
     default:
       result(FlutterMethodNotImplemented)
     }
--- github-client/window_to_front/test/widget_test.dart
+++ github-client/window_to_front/test/widget_test.dart
@@ -12,13 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'dart:async';
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-class WindowToFront {
-  static const MethodChannel _channel = MethodChannel('window_to_front');
-  static Future<void> activate() {
-    return _channel.invokeMethod('activate');
-  }
+void main() {
+  // Nothing to do here.
+  test('Do nothing test', () {});
 }
