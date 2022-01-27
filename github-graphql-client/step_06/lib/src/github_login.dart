4581ad2441f7f567cc1424988a556fca51c2e344
Miguel Beltran <m@beltran.work>
2022-01-27 06:13:36 +0100

    add old github-graphql-client folder

---
+++ github-graphql-client/step_06/lib/src/github_login.dart
@@ -0,0 +1,137 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:oauth2/oauth2.dart' as oauth2;
+import 'package:url_launcher/url_launcher.dart';
+
+final _authorizationEndpoint =
+    Uri.parse('https://github.com/login/oauth/authorize');
+final _tokenEndpoint = Uri.parse('https://github.com/login/oauth/access_token');
+
+class GithubLoginWidget extends StatefulWidget {
+  const GithubLoginWidget({
+    required this.builder,
+    required this.githubClientId,
+    required this.githubClientSecret,
+    required this.githubScopes,
+    Key? key,
+  }) : super(key: key);
+  final AuthenticatedBuilder builder;
+  final String githubClientId;
+  final String githubClientSecret;
+  final List<String> githubScopes;
+
+  @override
+  _GithubLoginState createState() => _GithubLoginState();
+}
+
+typedef AuthenticatedBuilder = Widget Function(
+    BuildContext context, oauth2.Client client);
+
+class _GithubLoginState extends State<GithubLoginWidget> {
+  HttpServer? _redirectServer;
+  oauth2.Client? _client;
+
+  @override
+  Widget build(BuildContext context) {
+    final client = _client;
+    if (client != null) {
+      return widget.builder(context, client);
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Github Login'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            await _redirectServer?.close();
+            // Bind to an ephemeral port on localhost
+            _redirectServer = await HttpServer.bind('localhost', 0);
+            var authenticatedHttpClient = await _getOAuth2Client(
+                Uri.parse('http://localhost:${_redirectServer!.port}/auth'));
+            setState(() {
+              _client = authenticatedHttpClient;
+            });
+          },
+          child: const Text('Login to Github'),
+        ),
+      ),
+    );
+  }
+
+  Future<oauth2.Client> _getOAuth2Client(Uri redirectUrl) async {
+    if (widget.githubClientId.isEmpty || widget.githubClientSecret.isEmpty) {
+      throw const GithubLoginException(
+          'githubClientId and githubClientSecret must be not empty. '
+          'See `lib/github_oauth_credentials.dart` for more detail.');
+    }
+    var grant = oauth2.AuthorizationCodeGrant(
+      widget.githubClientId,
+      _authorizationEndpoint,
+      _tokenEndpoint,
+      secret: widget.githubClientSecret,
+      httpClient: _JsonAcceptingHttpClient(),
+    );
+    var authorizationUrl =
+        grant.getAuthorizationUrl(redirectUrl, scopes: widget.githubScopes);
+
+    await _redirect(authorizationUrl);
+    var responseQueryParameters = await _listen();
+    var client =
+        await grant.handleAuthorizationResponse(responseQueryParameters);
+    return client;
+  }
+
+  Future<void> _redirect(Uri authorizationUrl) async {
+    var url = authorizationUrl.toString();
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw GithubLoginException('Could not launch $url');
+    }
+  }
+
+  Future<Map<String, String>> _listen() async {
+    var request = await _redirectServer!.first;
+    var params = request.uri.queryParameters;
+    request.response.statusCode = 200;
+    request.response.headers.set('content-type', 'text/plain');
+    request.response.writeln('Authenticated! You can close this tab.');
+    await request.response.close();
+    await _redirectServer!.close();
+    _redirectServer = null;
+    return params;
+  }
+}
+
+class _JsonAcceptingHttpClient extends http.BaseClient {
+  final _httpClient = http.Client();
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers['Accept'] = 'application/json';
+    return _httpClient.send(request);
+  }
+}
+
+class GithubLoginException implements Exception {
+  const GithubLoginException(this.message);
+  final String message;
+  @override
+  String toString() => message;
+}
---
+++ github-graphql-client/window_to_front/lib/window_to_front.dart
@@ -0,0 +1,24 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class WindowToFront {
+  static const MethodChannel _channel = MethodChannel('window_to_front');
+  static Future<void> activate() {
+    return _channel.invokeMethod('activate');
+  }
+}
