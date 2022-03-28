bcaee3b5cd150fe6e4086ad5a97d9d8264c406fa
Brett Morgan <brett.morgan@gmail.com>
2022-03-29 08:58:03 +1000

    Disable `testing_codelab` on beta

--- flutter_ci_script_beta.sh
+++ flutter_ci_script_beta.sh
@@ -18,7 +18,8 @@ declare -a CODELABS=(
   "photos-sharing"
   "star_counter"
   "startup_namer"
-  "testing_codelab"
+  # TODO(domesticmouse): re-enable once stable inncrements
+  # "testing_codelab"
   "tfserving-flutter"
   "webview_flutter"
   )
