094d135327a6338f068ed2129f684532ffeec255
Brett Morgan <brett.morgan@gmail.com>
2022-04-23 13:10:57 +0000

    refactor

--- tooling/codelab_rebuild/bin/codelab_rebuild.dart
+++ tooling/codelab_rebuild/bin/codelab_rebuild.dart
@@ -1,24 +1,7 @@
-import 'dart:io';
-
-import 'package:checked_yaml/checked_yaml.dart';
-import 'package:codelab_rebuild/configuration.dart';
+import 'package:codelab_rebuild/load_config.dart';
 
 void main(List<String> arguments) {
   final sourcePathOrYaml = arguments.single;
-  String yamlContent;
-  Uri? sourceUri;
-
-  if (FileSystemEntity.isFileSync(sourcePathOrYaml)) {
-    yamlContent = File(sourcePathOrYaml).readAsStringSync();
-    sourceUri = Uri.parse(sourcePathOrYaml);
-  } else {
-    yamlContent = sourcePathOrYaml;
-  }
-
-  final config = checkedYamlDecode(
-    yamlContent,
-    (m) => Configuration.fromJson(m!),
-    sourceUrl: sourceUri,
-  );
+  Configuration config = loadConfig(sourcePathOrYaml);
   print(config);
 }
