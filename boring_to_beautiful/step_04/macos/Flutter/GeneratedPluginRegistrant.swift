84aa97113b708fac1343556d73e7d14cf0d0226d
Brett Morgan <brett.morgan@gmail.com>
2022-06-01 06:15:57 +0000

    `boring_to_beautiful` dep updates

--- boring_to_beautiful/step_04/macos/Flutter/GeneratedPluginRegistrant.swift
+++ boring_to_beautiful/step_04/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import dynamic_color
 import path_provider_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
