2915351bd70d33f11ca5b6de184cfe7995c621cb
Brett Morgan <brett.morgan@gmail.com>
2022-06-02 04:14:22 +1000

    Drop `package-lock.json` (#903)

--- boring_to_beautiful/step_02/windows/flutter/generated_plugin_registrant.cc
+++ boring_to_beautiful/step_02/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <dynamic_color/dynamic_color_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  DynamicColorPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DynamicColorPluginCApi"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
