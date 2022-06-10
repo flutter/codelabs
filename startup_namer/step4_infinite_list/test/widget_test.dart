baf6a8d8b4870be7abc36d4821e06ae3406f8fe3
Brett Morgan <brettmorgan@google.com>
2022-06-10 23:04:59 +1000

    Fixup

--- startup_namer/step4_infinite_list/test/widget_test.dart
+++ startup_namer/step4_infinite_list/test/widget_test.dart
@@ -14,12 +14,14 @@ void main() {
     await tester.pumpWidget(const MyApp());
 
     final textWidgets = tester.widgetList<Text>(find.byType(Text));
-    expect(textWidgets.length, 2);
+    expect(textWidgets.length, greaterThan(2));
 
-    expect(textWidgets.last.data, 'Welcome to Flutter');
+    expect(textWidgets.last.data, 'Startup Name Generator');
 
     final wordPairRegExp = RegExp(r'^[A-Z]\w*[A-Z]\w*$');
     final isWordPair = predicate<String>((s) => wordPairRegExp.hasMatch(s));
-    expect(textWidgets.first.data, isWordPair);
+    for (final widget in textWidgets.take(textWidgets.length - 1)) {
+      expect(widget.data, isWordPair);
+    }
   });
 }
