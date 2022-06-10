31f909d915a27aeda0ceb6810500aff191cfbcb4
Brett Morgan <brettmorgan@google.com>
2022-06-10 15:26:14 +1000

    `startup_namer` Add codelab rebuild script

--- startup_namer/step4_infinite_list/test/widget_test.dart
+++ startup_namer/step4_infinite_list/test/widget_test.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Basic Flutter widget test. Learn more at https://flutter.dev/docs/testing.
+// Basic Flutter widget test. Learn more at https://flutter.dev/docs/testing
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -14,14 +14,12 @@ void main() {
     await tester.pumpWidget(const MyApp());
 
     final textWidgets = tester.widgetList<Text>(find.byType(Text));
-    expect(textWidgets.length, greaterThan(2));
+    expect(textWidgets.length, 2);
 
-    expect(textWidgets.last.data, 'Startup Name Generator');
+    expect(textWidgets.last.data, 'Welcome to Flutter');
 
     final wordPairRegExp = RegExp(r'^[A-Z]\w*[A-Z]\w*$');
     final isWordPair = predicate<String>((s) => wordPairRegExp.hasMatch(s));
-    for (final widget in textWidgets.take(textWidgets.length - 1)) {
-      expect(widget.data, isWordPair);
-    }
+    expect(textWidgets.first.data, isWordPair);
   });
 }
