eb661b0e07ecac4e86673cd5b56c91cea3b77ade
Khanh Nguyen <khanhnwin@gmail.com>
2022-07-24 16:34:48 -0700

    set minimum window size on desktop (#1037)

--- boring_to_beautiful/step_04/lib/main.dart
+++ boring_to_beautiful/step_04/lib/main.dart
@@ -1,7 +1,21 @@
+import 'dart:io' show Platform;
+
+import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 
 import 'src/shared/app.dart';
 
+Future setDesktopWindow() async {
+  await DesktopWindow.setMinWindowSize(const Size(400, 400));
+  await DesktopWindow.setWindowSize(const Size(1300, 900));
+}
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setDesktopWindow();
+  }
+
   runApp(const MyApp());
 }
