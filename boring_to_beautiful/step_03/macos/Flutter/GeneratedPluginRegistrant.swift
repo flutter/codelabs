155ef5a47fd21d4c870063cd5cdc76ad3ec0ee53
Brett Morgan <brettmorgan@google.com>
2022-08-01 23:17:09 +1000

    `boring_to_beautiful` Update macOS Runner with `desktop_window`

--- boring_to_beautiful/step_03/macos/Flutter/GeneratedPluginRegistrant.swift
+++ boring_to_beautiful/step_03/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import desktop_window
 import dynamic_color
 import path_provider_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
