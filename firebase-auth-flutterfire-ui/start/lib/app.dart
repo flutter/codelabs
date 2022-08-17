92a71a061a6989fec73bad6270e85ea473e3db03
Eric Windmill <ewindmill@google.com>
2022-08-17 12:39:49 -0700

    adds start folder

---
+++ firebase-auth-flutterfire-ui/start/lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'auth_gate.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AuthGate(),
+    );
+  }
+}
