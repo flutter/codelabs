3afbee57a0c0aa81a9e9dcf9a21449a8ae3d32e9
Brett Morgan <brett.morgan@gmail.com>
2022-09-01 09:56:37 +1000

    Elide `adaptive_app` from Beta CI (#1079)

--- flutter_ci_script_beta.sh
+++ flutter_ci_script_beta.sh
@@ -6,7 +6,8 @@ DIR="${BASH_SOURCE%/*}"
 source "$DIR/flutter_ci_script_shared.sh"
 
 declare -a CODELABS=(
-  "adaptive_app"
+  # TODO(domesticmouse): Re-enable when Flutter Beta >= 3.3.0
+  # "adaptive_app"
   "boring_to_beautiful"
   "cookbook"
   "cupertino_store"
