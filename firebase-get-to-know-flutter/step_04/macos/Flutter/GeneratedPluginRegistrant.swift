5adad639c3da6d2495ffde267713eb8a361349b4
Alexander Nohe <nohe@google.com>
2022-09-12 14:23:35 -0700

    Add in flutterfire_ui for a modified codelab to use new features.

--- firebase-get-to-know-flutter/step_04/macos/Flutter/GeneratedPluginRegistrant.swift
+++ firebase-get-to-know-flutter/step_04/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,19 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import desktop_webview_auth
 import firebase_auth
 import firebase_core
+import firebase_database
 import path_provider_macos
+import sign_in_with_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
 }
