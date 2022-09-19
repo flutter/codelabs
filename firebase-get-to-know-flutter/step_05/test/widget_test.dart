ff19f6dba0a825c6027d88d3bba172208b57d40e
Alexander Nohe <nohe@google.com>
2022-09-19 16:42:57 -0700

    Widget test fixes

--- firebase-get-to-know-flutter/step_05/test/widget_test.dart
+++ firebase-get-to-know-flutter/step_05/test/widget_test.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:gtk_flutter/firebase_options.dart';
 import 'package:gtk_flutter/main.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   testWidgets('Basic rendering', (tester) async {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
     // Build our app and trigger a frame.
     await tester.pumpWidget(
       ChangeNotifierProvider(
