b4c9c2cb23db3744395f93f6e4e75cbd5dce5b16
Alexander Nohe <nohe427@users.noreply.github.com>
2022-10-07 14:39:07 -0700

    Adding in supplemental material for Firebase Summit 2022 (#1112)

---
+++ firebase-get-to-know-flutter/supplemental/part_03/src/lib/src/authentication.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'widgets.dart';
+
+class AuthFunc extends StatelessWidget {
+  const AuthFunc({
+    super.key,
+    required this.loggedIn,
+    required this.signOut,
+    required this.enableFreeSwag,
+  });
+
+  final bool loggedIn;
+  final void Function() signOut;
+  final bool enableFreeSwag;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 24, bottom: 8),
+          child: StyledButton(
+              onPressed: () {
+                !loggedIn
+                    ? Navigator.of(context).pushNamed('/sign-in')
+                    : signOut();
+              },
+              child: !loggedIn ? const Text('RSVP') : const Text('Logout')),
+        ),
+        Visibility(
+            visible: loggedIn,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 24, bottom: 8),
+              child: StyledButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/profile');
+                  },
+                  child: const Text('Profile')),
+            )),
+        Visibility(
+            visible: enableFreeSwag,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 24, bottom: 8),
+              child: StyledButton(
+                  onPressed: () {
+                    throw Exception('swag tool unimplemented');
+                  },
+                  child: const Text('Free Swag!')),
+            )),
+      ],
+    );
+  }
+}
