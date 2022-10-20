5ba16a8f1307d661161617b98639f63c47a1f2fd
maupande <pandeymauli@gmail.com>
2022-10-20 18:08:00 -0400

    added code excerpt to widget_test.dart

--- startup_namer/step3_stateful_widget/test/widget_test.dart
+++ startup_namer/step3_stateful_widget/test/widget_test.dart
@@ -21,5 +21,22 @@ void main() {
     final wordPairRegExp = RegExp(r'^[A-Z]\w*[A-Z]\w*$');
     final isWordPair = predicate<String>((s) => wordPairRegExp.hasMatch(s));
     expect(textWidgets.first.data, isWordPair);
+
+    final SemanticsHandle handle = tester.ensureSemantics();
+    await tester.pumpWidget(const MaterialApp(home: MyApp()));
+
+    // Checks that tappable nodes have a minimum size of 48 by 48 pixels for android.
+    await expectLater(tester, meetsGuideline(androidTapTargetGuideline));
+
+    // Checks that tappable nodes have a minimum size of 44 by 44 pixels for iOS.
+    await expectLater(tester, meetsGuideline(iOSTapTargetGuideline));
+
+    // Checks that touch targets with a tap or long press action are labeled.
+    await expectLater(tester, meetsGuideline(labeledTapTargetGuideline));
+
+    // Checks whether semantic nodes meet the minimum text contrast levels.
+    // The recommended text contrast is 3:1 for larger text (18 point and above regular)
+    await expectLater(tester, meetsGuideline(textContrastGuideline));
+    handle.dispose();
   });
 }
