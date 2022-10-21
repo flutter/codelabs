832a6dc7491e85aee133f982b24b1bb53ac1e7ee
Brett Morgan <brettmorgan@google.com>
2022-10-21 15:23:04 -0400

    Update `firebase-get-to-know-flutter`

--- firebase-get-to-know-flutter/step_06/macos/Flutter/GeneratedPluginRegistrant.swift
+++ firebase-get-to-know-flutter/step_06/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import cloud_firestore
 import desktop_webview_auth
 import firebase_auth
 import firebase_core
-import firebase_database
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -17,6 +16,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
