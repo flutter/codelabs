31b239b816bfb7cebb59e2250dcf784e46402ae1
Miguel Beltran <m@beltran.work>
2022-11-23 15:54:05 +0100

    add go_route to step_04

--- adaptive_app/step_04/lib/main.dart
+++ adaptive_app/step_04/lib/main.dart
@@ -2,9 +2,11 @@ import 'dart:io';
 
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 import 'src/app_state.dart';
+import 'src/playlist_details.dart';
 import 'src/playlists.dart';
 
 // From https://www.youtube.com/channel/UCwXdFgeE9KYzlDdR7TG9cMw
@@ -12,6 +14,30 @@ const flutterDevAccountId = 'UCwXdFgeE9KYzlDdR7TG9cMw';
 
 // TODO: Replace with your YouTube API Key
 const youTubeApiKey = 'AIzaNotAnApiKey';
+
+final _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (context, state) {
+        return const Playlists();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'playlist/:id',
+          builder: (context, state) {
+            final title = state.queryParams['title']!;
+            final id = state.params['id']!;
+            return PlaylistDetails(
+              playlistId: id,
+              playlistName: title,
+            );
+          },
+        ),
+      ],
+    ),
+  ],
+);
 
 void main() {
   if (youTubeApiKey == 'AIzaNotAnApiKey') {
@@ -33,13 +59,13 @@ class PlaylistsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'FlutterDev Playlists',
       theme: FlexColorScheme.light(scheme: FlexScheme.red).toTheme,
       darkTheme: FlexColorScheme.dark(scheme: FlexScheme.red).toTheme,
       themeMode: ThemeMode.dark, // Or ThemeMode.System if you'd prefer
       debugShowCheckedModeBanner: false,
-      home: const Playlists(),
+      routerConfig: _router,
     );
   }
 }
--- adaptive_app/step_04/lib/src/playlists.dart
+++ adaptive_app/step_04/lib/src/playlists.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:googleapis/youtube/v3.dart';
 import 'package:provider/provider.dart';
 
@@ -52,16 +53,13 @@ class _PlaylistsListView extends StatelessWidget {
               playlist.snippet!.description!,
             ),
             onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute<void>(
-                  builder: (context) {
-                    return PlaylistDetails(
-                      playlistId: playlist.id!,
-                      playlistName: playlist.snippet!.title!,
-                    );
+              context.go(
+                Uri(
+                  path: '/playlist/${playlist.id}',
+                  queryParameters: <String, String>{
+                    'title': playlist.snippet!.title!
                   },
-                ),
+                ).toString(),
               );
             },
           ),
