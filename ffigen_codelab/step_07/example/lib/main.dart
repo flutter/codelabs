ae01e5d3013af4f8b5a235e60020de130c78eeee
Brett Morgan <brettmorgan@google.com>
2022-12-02 17:55:48 +1000

    Start of Duktape REPL

--- ffigen_codelab/step_07/example/lib/main.dart
+++ ffigen_codelab/step_07/example/lib/main.dart
@@ -1,81 +1,102 @@
 import 'package:ffigen_app/ffigen_app.dart';
 import 'package:flutter/material.dart';
-
-const String jsCode = '''
-var foo = "foo";
-var bar = "bar";
-foo + bar;
-''';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'duktape_message.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatefulWidget {
+final duktapeMessagesProvider =
+    StateNotifierProvider<DuktapeMessageNotifier, List<DuktapeMessage>>((ref) {
+  return DuktapeMessageNotifier(messages: <DuktapeMessage>[]);
+});
+
+class DuktapeMessageNotifier extends StateNotifier<List<DuktapeMessage>> {
+  DuktapeMessageNotifier({required List<DuktapeMessage> messages})
+      : duktape = Duktape(),
+        super(messages);
+  final Duktape duktape;
+
+  void eval(String code) {
+    state = [
+      DuktapeMessage.evaluate(code),
+      ...state,
+    ];
+    try {
+      final response = duktape.evalString(code);
+      state = [
+        DuktapeMessage.response(response),
+        ...state,
+      ];
+    } catch (e) {
+      state = [
+        DuktapeMessage.error('$e'),
+        ...state,
+      ];
+    }
+  }
+}
+
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  MyAppState createState() => MyAppState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final messages = ref.watch(duktapeMessagesProvider);
 
-class MyAppState extends State<MyApp> {
-  late Duktape duktape;
-  String output = '';
-
-  @override
-  void initState() {
-    super.initState();
-    duktape = Duktape();
-    setState(() {
-      output = 'Initialized Duktape';
-    });
-  }
-
-  @override
-  void dispose() {
-    duktape.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    const textStyle = TextStyle(fontSize: 25);
-    const spacerSmall = SizedBox(height: 10);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Duktape Test'),
+          title: const Text('Duktape REPL'),
         ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  output,
-                  style: textStyle,
-                  textAlign: TextAlign.center,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView.builder(
+            itemBuilder: (context, idx) => messages[idx].when(
+              evaluate: (str) => Padding(
+                padding: const EdgeInsets.only(top: 2, bottom: 1),
+                child: Text(
+                  '> $str',
+                  style: GoogleFonts.inconsolata(
+                    textStyle: Theme.of(context).textTheme.bodyLarge,
+                  ),
                 ),
-                spacerSmall,
-                ElevatedButton(
-                  child: const Text('Run JavaScript'),
-                  onPressed: () {
-                    try {
-                      final result = duktape.evalString(jsCode);
-                      setState(() {
-                        output = '$jsCode => $result';
-                      });
-                    } catch (e) {
-                      setState(() {
-                        output = '$e';
-                      });
-                    }
-                  },
+              ),
+              response: (str) => Padding(
+                padding: const EdgeInsets.only(top: 1, bottom: 2),
+                child: Text(
+                  '= $str',
+                  style: GoogleFonts.inconsolata(
+                    textStyle: Theme.of(context).textTheme.bodyLarge,
+                    color: Colors.blue[800],
+                  ),
                 ),
-              ],
+              ),
+              error: (str) => Padding(
+                padding: const EdgeInsets.symmetric(vertical: 2),
+                child: Text(
+                  '! $str',
+                  style: GoogleFonts.inconsolata(
+                    textStyle: Theme.of(context).textTheme.bodyLarge,
+                    color: Colors.red[600],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ),
+            itemCount: messages.length,
+            reverse: true,
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            ref
+                .read(duktapeMessagesProvider.notifier)
+                .eval('var foo = 1; foo + 2');
+          },
         ),
       ),
     );
