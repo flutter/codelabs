f200a8fd70fccf0fbc651fd08f5eea1829724ad3
Brett Morgan <brettmorgan@google.com>
2022-12-04 21:36:29 +1000

    Fixup `widget_test.dart`

--- ffigen_codelab/step_07/example/test/widget_test.dart
+++ ffigen_codelab/step_07/example/test/widget_test.dart
@@ -1,7 +1,7 @@
+import 'package:ffigen_app/ffigen_app.dart';
 import 'package:ffigen_app_example/main.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ffigen_app/ffigen_app.dart';
 
 void main() {
   testWidgets('DuktapeApp displays the title', (tester) async {
