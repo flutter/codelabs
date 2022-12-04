bfe62624230c5163696ef4eefaf851f0ebfdbcd3
Brett Morgan <brettmorgan@google.com>
2022-12-04 20:41:12 +1000

    Test library binding WIP

--- ffigen_codelab/step_07/example/test/widget_test.dart
+++ ffigen_codelab/step_07/example/test/widget_test.dart
@@ -1,6 +1,7 @@
 import 'package:ffigen_app_example/main.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:ffigen_app/ffigen_app.dart';
 
 void main() {
   testWidgets('DuktapeApp displays the title', (tester) async {
@@ -9,5 +10,11 @@ void main() {
 
     // Verify that our app displays the title
     expect(find.text('Duktape App'), findsOneWidget);
+  });
+
+  test('Duktape.evalString', () {
+    final duktape = Duktape();
+    final response = duktape.evalString('1+2');
+    expect(response, '3');
   });
 }
