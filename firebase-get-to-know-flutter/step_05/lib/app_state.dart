b5dd5c61ee4cfc19a2c513debf8d8d5788fd2107
Miguel Beltran <m@beltran.work>
2022-12-07 13:47:58 +0100

    add license headers

--- firebase-get-to-know-flutter/step_05/lib/app_state.dart
+++ firebase-get-to-know-flutter/step_05/lib/app_state.dart
@@ -1,3 +1,7 @@
+// Copyright 2022 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:firebase_auth/firebase_auth.dart'
     hide EmailAuthProvider, PhoneAuthProvider;
 import 'package:firebase_core/firebase_core.dart';
--- firebase-get-to-know-flutter/step_09/lib/main.dart
+++ firebase-get-to-know-flutter/step_09/lib/main.dart
@@ -1,3 +1,7 @@
+// Copyright 2022 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
