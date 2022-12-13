10c56aa774437f677da3b570de40f2720791c754
Brett Morgan <brettmorgan@google.com>
2022-12-13 15:06:51 +1000

    `MyAppState` -> `_MyAppState`

--- ffigen_codelab/step_03/example/lib/main.dart
+++ ffigen_codelab/step_03/example/lib/main.dart
@@ -11,10 +11,10 @@ class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  MyAppState createState() => MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> {
   late int sumResult;
   late Future<int> sumAsyncResult;
 
