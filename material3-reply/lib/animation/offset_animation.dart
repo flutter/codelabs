6b4007895a33ff0970f3339a3835de23dec04344
Alex Wallen <wallenstephen@outlook.com>
2022-12-13 23:29:16 +0000

    Adaptive Demo [Material 3 Reply App] (#1214)

---
+++ material3-reply/lib/animation/offset_animation.dart
@@ -0,0 +1,22 @@
+// Copyright 2022 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/animation.dart';
+
+class OffsetAnimation extends CurvedAnimation {
+  OffsetAnimation(Animation<double> parent)
+      : super(
+          parent: parent,
+          curve: const Interval(
+            2 / 5,
+            3 / 5,
+            curve: Curves.easeInOutCubicEmphasized,
+          ),
+          reverseCurve: Interval(
+            4 / 5,
+            1,
+            curve: Curves.easeInOutCubicEmphasized.flipped,
+          ),
+        );
+}
