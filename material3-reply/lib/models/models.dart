8387e4deda39ad4d8ec42004c5419c43172ccb8d
Brett Morgan <brett.morgan@gmail.com>
2022-12-14 17:28:04 +1100

    Bring in repo standard analysis options (#1216)
    * Bring in repo standard analysis options
    
    * dart format

--- material3-reply/lib/models/models.dart
+++ material3-reply/lib/models/models.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export 'attachment.dart';
 export 'email.dart';
 export 'name.dart';
 export 'user.dart';
-export 'attachment.dart';
