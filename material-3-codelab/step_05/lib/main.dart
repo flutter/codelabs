f6c392d89541a23d050ffe76ca7bb11ddef6cea8
Brett Morgan <brettmorgan@google.com>
2022-12-15 15:57:54 +1100

    Step 5

--- material-3-codelab/step_05/lib/main.dart
+++ material-3-codelab/step_05/lib/main.dart
@@ -1,4 +1,12 @@
+// Copyright 2022 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
+
+import 'models/data.dart' as data;
+import 'models/models.dart';
+import 'widgets/email_list_view.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +17,42 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData.light(useMaterial3: true),
+      home: Feed(currentUser: data.user_0),
+    );
+  }
+}
+
+class Feed extends StatefulWidget {
+  const Feed({
+    super.key,
+    required this.currentUser,
+  });
+
+  final User currentUser;
+
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> with SingleTickerProviderStateMixin {
+  late final colorScheme = Theme.of(context).colorScheme;
+  late final backgroundColor = Color.alphaBlend(
+      colorScheme.primary.withOpacity(0.14), colorScheme.surface);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: backgroundColor,
+        child: EmailListView(
+          currentUser: widget.currentUser,
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
