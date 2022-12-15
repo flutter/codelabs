d90e1af81b09dabded886215abacccb56f31c266
Brett Morgan <brettmorgan@google.com>
2022-12-15 23:02:53 +1100

    tidy up test

--- material3-reply/test/widget_test.dart
+++ material3-reply/test/widget_test.dart
@@ -1,10 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:material3_reply/main.dart';
 
 void main() {
-  testWidgets('Do nothing test', (tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Build app and trigger a frame', (tester) async {
     await tester.pumpWidget(const MainApp());
   });
 }
