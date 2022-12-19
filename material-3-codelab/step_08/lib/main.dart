7af14aaf7e5f381fcbdda47976ae76c752f491ba
Brett Morgan <brettmorgan@google.com>
2022-12-19 21:28:32 +1000

    Step 8, part 2

--- material-3-codelab/step_08/lib/main.dart
+++ material-3-codelab/step_08/lib/main.dart
@@ -4,8 +4,12 @@
 
 import 'package:flutter/material.dart';
 
+import 'animation/bar_animation.dart';
+import 'animation/rail_animation.dart';
+import 'animation/rail_fab_animation.dart';
 import 'models/data.dart' as data;
 import 'models/models.dart';
+import 'widgets/animated_floating_action_button.dart';
 import 'widgets/disappearing_bottom_navigation_bar.dart';
 import 'widgets/disappearing_navigation_rail.dart';
 import 'widgets/email_list_view.dart';
@@ -38,20 +42,49 @@ class Feed extends StatefulWidget {
   State<Feed> createState() => _FeedState();
 }
 
-class _FeedState extends State<Feed> {
+class _FeedState extends State<Feed> with SingleTickerProviderStateMixin {
   late final colorScheme = Theme.of(context).colorScheme;
   late final backgroundColor = Color.alphaBlend(
       colorScheme.primary.withOpacity(0.14), colorScheme.surface);
+  late final controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      reverseDuration: const Duration(milliseconds: 1250),
+      value: 0,
+      vsync: this);
+  late final railAnimation = RailAnimation(parent: controller);
+  late final railFabAnimation = RailFabAnimation(parent: controller);
+  late final barAnimation = BarAnimation(parent: controller);
 
   int selectedIndex = 0;
-  bool wideScreen = false;
+  bool controllerInitialized = false;
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
 
     final double width = MediaQuery.of(context).size.width;
-    wideScreen = width > 600;
+    final AnimationStatus status = controller.status;
+    if (width > 600) {
+      if (status != AnimationStatus.forward &&
+          status != AnimationStatus.completed) {
+        controller.forward();
+      }
+    } else {
+      if (status != AnimationStatus.reverse &&
+          status != AnimationStatus.dismissed) {
+        controller.reverse();
+      }
+    }
+    if (!controllerInitialized) {
+      controllerInitialized = true;
+      controller.value = width > 600 ? 1 : 0;
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -59,16 +92,17 @@ class _FeedState extends State<Feed> {
     return Scaffold(
       body: Row(
         children: [
-          if (wideScreen)
-            DisappearingNavigationRail(
-              selectedIndex: selectedIndex,
-              backgroundColor: backgroundColor,
-              onDestinationSelected: (index) {
-                setState(() {
-                  selectedIndex = index;
-                });
-              },
-            ),
+          DisappearingNavigationRail(
+            railAnimation: railAnimation,
+            railFabAnimation: railFabAnimation,
+            selectedIndex: selectedIndex,
+            backgroundColor: backgroundColor,
+            onDestinationSelected: (index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+          ),
           Expanded(
             child: Container(
               color: backgroundColor,
@@ -85,29 +119,20 @@ class _FeedState extends State<Feed> {
           ),
         ],
       ),
-      floatingActionButton: wideScreen
-          ? null
-          : FloatingActionButton(
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(15),
-                ),
-              ),
-              backgroundColor: colorScheme.tertiaryContainer,
-              foregroundColor: colorScheme.onTertiaryContainer,
-              onPressed: () {},
-              child: const Icon(Icons.add),
-            ),
-      bottomNavigationBar: wideScreen
-          ? null
-          : DisappearingBottomNavigationBar(
-              selectedIndex: selectedIndex,
-              onDestinationSelected: (index) {
-                setState(() {
-                  selectedIndex = index;
-                });
-              },
-            ),
+      floatingActionButton: AnimatedFloatingActionButton(
+        animation: barAnimation,
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: DisappearingBottomNavigationBar(
+        barAnimation: barAnimation,
+        selectedIndex: selectedIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+      ),
     );
   }
 }
--- material-3-codelab/step_08/lib/widgets/disappearing_bottom_navigation_bar.dart
+++ material-3-codelab/step_08/lib/widgets/disappearing_bottom_navigation_bar.dart
@@ -4,31 +4,39 @@
 
 import 'package:flutter/material.dart';
 
+import '../animation/bar_animation.dart';
 import '../destinations.dart';
+import '../transitions/bottom_bar_transition.dart';
 
 class DisappearingBottomNavigationBar extends StatelessWidget {
   const DisappearingBottomNavigationBar({
     super.key,
+    required this.barAnimation,
     required this.selectedIndex,
     this.onDestinationSelected,
   });
 
+  final BarAnimation barAnimation;
   final int selectedIndex;
   final ValueChanged<int>? onDestinationSelected;
 
   @override
   Widget build(BuildContext context) {
-    return NavigationBar(
-      elevation: 0,
+    return BottomBarTransition(
+      animation: barAnimation,
       backgroundColor: Colors.white,
-      destinations: destinations.map<NavigationDestination>((d) {
-        return NavigationDestination(
-          icon: Icon(d.icon),
-          label: d.label,
-        );
-      }).toList(),
-      selectedIndex: selectedIndex,
-      onDestinationSelected: onDestinationSelected,
+      child: NavigationBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        destinations: destinations.map<NavigationDestination>((d) {
+          return NavigationDestination(
+            icon: Icon(d.icon),
+            label: d.label,
+          );
+        }).toList(),
+        selectedIndex: selectedIndex,
+        onDestinationSelected: onDestinationSelected,
+      ),
     );
   }
 }
