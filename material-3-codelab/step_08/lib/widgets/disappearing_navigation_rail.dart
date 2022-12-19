e42c1a2b2588a81265738159755f19ee1dc1085e
Brett Morgan <brettmorgan@google.com>
2022-12-19 21:14:57 +1000

    Step 8, part 1

---
+++ material-3-codelab/step_08/lib/widgets/disappearing_navigation_rail.dart
@@ -0,0 +1,57 @@
+// Copyright 2022 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+import '../destinations.dart';
+
+class DisappearingNavigationRail extends StatelessWidget {
+  const DisappearingNavigationRail({
+    super.key,
+    required this.backgroundColor,
+    required this.selectedIndex,
+    this.onDestinationSelected,
+  });
+
+  final Color backgroundColor;
+  final int selectedIndex;
+  final ValueChanged<int>? onDestinationSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return NavigationRail(
+      selectedIndex: selectedIndex,
+      backgroundColor: backgroundColor,
+      onDestinationSelected: onDestinationSelected,
+      leading: Column(
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(15),
+              ),
+            ),
+            backgroundColor: colorScheme.tertiaryContainer,
+            foregroundColor: colorScheme.onTertiaryContainer,
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      groupAlignment: -0.85,
+      destinations: destinations.map((d) {
+        return NavigationRailDestination(
+          icon: Icon(d.icon),
+          label: Text(d.label),
+        );
+      }).toList(),
+    );
+  }
+}
