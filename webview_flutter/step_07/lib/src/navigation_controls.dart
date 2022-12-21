dfdcc4bce8f88bd238effb9e381d954eff139153
Brett Morgan <brett.morgan@gmail.com>
2022-12-21 13:29:58 +1100

    Update for Flutter 3.3.10 (#1252)
    * Update `pubspec.yaml`s
    
    * Update `firebase-get-to-know-flutter`
    
    * `github-client`
    
    * `google-maps-in-flutter`
    
    * `star_counter`
    
    * `testing_codelab`
    
    * `webview_flutter`
    
    * Drop `webview_flutter` test
    
    * Update deps

--- webview_flutter/step_07/lib/src/navigation_controls.dart
+++ webview_flutter/step_07/lib/src/navigation_controls.dart
@@ -1,68 +1,50 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class NavigationControls extends StatelessWidget {
   const NavigationControls({required this.controller, super.key});
 
-  final Completer<WebViewController> controller;
+  final WebViewController controller;
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<WebViewController>(
-      future: controller.future,
-      builder: (context, snapshot) {
-        final WebViewController? controller = snapshot.data;
-        if (snapshot.connectionState != ConnectionState.done ||
-            controller == null) {
-          return Row(
-            children: const <Widget>[
-              Icon(Icons.arrow_back_ios),
-              Icon(Icons.arrow_forward_ios),
-              Icon(Icons.replay),
-            ],
-          );
-        }
-
-        return Row(
-          children: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.arrow_back_ios),
-              onPressed: () async {
-                final messenger = ScaffoldMessenger.of(context);
-                if (await controller.canGoBack()) {
-                  await controller.goBack();
-                } else {
-                  messenger.showSnackBar(
-                    const SnackBar(content: Text('No back history item')),
-                  );
-                  return;
-                }
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.arrow_forward_ios),
-              onPressed: () async {
-                final messenger = ScaffoldMessenger.of(context);
-                if (await controller.canGoForward()) {
-                  await controller.goForward();
-                } else {
-                  messenger.showSnackBar(
-                    const SnackBar(content: Text('No forward history item')),
-                  );
-                  return;
-                }
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.replay),
-              onPressed: () {
-                controller.reload();
-              },
-            ),
-          ],
-        );
-      },
+    return Row(
+      children: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () async {
+            final messenger = ScaffoldMessenger.of(context);
+            if (await controller.canGoBack()) {
+              await controller.goBack();
+            } else {
+              messenger.showSnackBar(
+                const SnackBar(content: Text('No back history item')),
+              );
+              return;
+            }
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.arrow_forward_ios),
+          onPressed: () async {
+            final messenger = ScaffoldMessenger.of(context);
+            if (await controller.canGoForward()) {
+              await controller.goForward();
+            } else {
+              messenger.showSnackBar(
+                const SnackBar(content: Text('No forward history item')),
+              );
+              return;
+            }
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.replay),
+          onPressed: () {
+            controller.reload();
+          },
+        ),
+      ],
     );
   }
 }
--- webview_flutter/step_09/lib/main.dart
+++ webview_flutter/step_09/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -23,7 +21,16 @@ class WebViewApp extends StatefulWidget {
 }
 
 class _WebViewAppState extends State<WebViewApp> {
-  final controller = Completer<WebViewController>();
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse('https://flutter.dev'),
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
