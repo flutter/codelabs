cb948f3dc7c12a59cc07619dcf2afecff0be23d5
Alexander Nohe <nohe@google.com>
2023-01-23 10:58:52 +0300

    Remove go_router

--- firebase-get-to-know-flutter/step_09/lib/main.dart
+++ firebase-get-to-know-flutter/step_09/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:firebase_auth/firebase_auth.dart'
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
