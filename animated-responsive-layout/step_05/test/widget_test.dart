31224754d7827dc8c35165df98a53858d98b9217
Brett Morgan <brett.morgan@gmail.com>
2023-01-25 10:14:33 +1100

    Flutter 3.7.0 (#1323)
    * Update `adaptive_app`
    
    * Update `boring_to_beautiful`
    
    * Update `cookbook`
    
    * Update `cupertino_store`
    
    * Update `ffigen_codelab`
    
    * Fixup `boring_to_beautiful`
    
    * Update `firebase-get-to-know-flutter`
    
    * Update `flame-building-doodle-dash`
    
    * Update `cookbook`
    
    * Update `firebase-get-to-know-flutter`
    
    * Update `github-client`
    
    * Update `google-maps-in-flutter`
    
    * `dart format tfserving-flutter`
    
    * Update `namer`
    
    * Update `star_counter`
    
    * Update `startup_namer`
    
    * Update `webview_flutter`
    
    * Update
    
    * Patch the script
    
    * Add back `outlineVariant` and `scrim`
    
    * Drop `yt_cors_proxy/pubspec.yaml`'s `repository` comment
    
    * Update `firebase-get-to-know-flutter` rebuild script
    
    * fixups
    
    * Update the list of codelabs
    
    * Fixup `animated-responsive-layout`

--- animated-responsive-layout/step_05/test/widget_test.dart
+++ animated-responsive-layout/step_05/test/widget_test.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:animated_responsive_layout/main.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Build app and trigger a frame', (tester) async {
