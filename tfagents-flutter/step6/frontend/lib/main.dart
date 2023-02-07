ffade372a70c5939d748af2a55b325222364c9ef
Miguel Beltran <m@beltran.work>
2023-02-07 05:12:32 +0100

    Migrate tfagents codelab to Material 3 (#1359)
    * Migrate tfagents codelab to Material 3
    
    * replaced ElevatedButton with FilledButton

--- tfagents-flutter/step6/frontend/lib/main.dart
+++ tfagents-flutter/step6/frontend/lib/main.dart
@@ -52,6 +52,7 @@ class _PlaneStrikeState extends State<PlaneStrike>
       title: 'TFAgents Flutter Sample App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
       home: _buildGameBody(),
     );
@@ -207,7 +208,7 @@ class _PlaneStrikeState extends State<PlaneStrike>
                 Padding(
                   padding: const EdgeInsets.only(
                       left: 0, top: 20, right: 0, bottom: 0),
-                  child: ElevatedButton(
+                  child: FilledButton(
                     onPressed: () {
                       _resetGame();
                       setState(() {});
