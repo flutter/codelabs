6079f7388b001a28e71c096e37fbcfd2eb34b99c
Miguel Beltran <m@beltran.work>
2023-02-09 17:09:43 +0100

    updated Step 08

---
+++ testing_codelab/step_08/integration_test/perf_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:testing_app/main.dart';
+
+void main() {
+  group('Testing App Performance', () {
+    final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+    binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
+
+    testWidgets('Scrolling test', (tester) async {
+      await tester.pumpWidget(const TestingApp());
+
+      final listFinder = find.byType(ListView);
+
+      await binding.traceAction(() async {
+        await tester.fling(listFinder, const Offset(0, -500), 10000);
+        await tester.pumpAndSettle();
+
+        await tester.fling(listFinder, const Offset(0, 500), 10000);
+        await tester.pumpAndSettle();
+      }, reportKey: 'scrolling_summary');
+    });
+  });
+}
---
+++ testing_codelab/step_08/test_driver/perf_driver.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_driver/flutter_driver.dart' as driver;
+import 'package:integration_test/integration_test_driver.dart';
+
+Future<void> main() {
+  return integrationDriver(
+    responseDataCallback: (data) async {
+      if (data != null) {
+        final timeline = driver.Timeline.fromJson(data['scrolling_summary']);
+        final summary = driver.TimelineSummary.summarize(timeline);
+        await summary.writeTimelineToFile(
+          'scrolling_summary',
+          pretty: true,
+          includeSummary: true,
+        );
+      }
+    },
+  );
+}
