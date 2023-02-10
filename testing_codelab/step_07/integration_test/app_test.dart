801138daf11ff8492c5aad8458a86adf7b5dd0bf
Miguel Beltran <m@beltran.work>
2023-02-10 07:51:34 +0100

    remove unused import

--- testing_codelab/step_07/integration_test/app_test.dart
+++ testing_codelab/step_07/integration_test/app_test.dart
@@ -4,7 +4,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 import 'package:testing_app/main.dart';
 
 void main() {
