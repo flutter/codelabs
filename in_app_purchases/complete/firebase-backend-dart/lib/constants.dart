57dbeecb3ec2de927505dc2ad25f8d2e37579f82
Miguel Beltran <m@beltran.work>
2023-02-28 14:09:11 +0100

    default constants.dart

--- in_app_purchases/complete/firebase-backend-dart/lib/constants.dart
+++ in_app_purchases/complete/firebase-backend-dart/lib/constants.dart
@@ -1,4 +1,7 @@
-const androidPackageId = 'com.example.iap';
-const appStoreSharedSecret = 'replace-me';
+const androidPackageId = 'your Android package ID';
+const appStoreIssuerId = 'App Store Key issuer ID';
+const appStoreKeyId = 'App Store Key ID';
+const appStoreSharedSecret = 'App Store shared secret';
+const bundleId = 'your iOS bundle ID';
+const googlePlayProjectName = 'Google Cloud project name';
 const googlePlayPubsubBillingTopic = 'play_billing';
-const googlePlayProjectName = 'replace-me';
