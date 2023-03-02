c29018b92335d15668e5e32884ca0030e6a216a1
Soliev <33078170+soliev01@users.noreply.github.com>
2023-03-02 11:44:17 +0900

    Update gradient_bubbles.dart
    Readability improvement

--- cookbook/lib/examples/gradient_bubbles.dart
+++ cookbook/lib/examples/gradient_bubbles.dart
@@ -85,14 +85,15 @@ class MessageBubble extends StatelessWidget {
           child: ClipRRect(
             borderRadius: const BorderRadius.all(Radius.circular(16.0)),
             child: BubbleBackground(
-              colors: [
-                message.isMine
-                    ? const Color(0xFF6C7689)
-                    : const Color(0xFF19B7FF),
-                message.isMine
-                    ? const Color(0xFF3A364B)
-                    : const Color(0xFF491CCB),
-              ],
+              colors: message.isMine
+                  ? [
+                      const Color(0xFF6C7689),
+                      const Color(0xFF3A364B),
+                    ]
+                  : [
+                      const Color(0xFF19B7FF),
+                      const Color(0xFF491CCB),
+                    ],
               child: DefaultTextStyle.merge(
                 style: const TextStyle(
                   fontSize: 18.0,
