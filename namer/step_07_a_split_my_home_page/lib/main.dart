2a5fe03c202a6b5741cbb0003924313c115ebc64
Filip Hracek <filiph@users.noreply.github.com>
2023-03-04 00:06:54 +0100

    Make an accessibility improvement (#1457)
    According to https://github.com/flutter/flutter/issues/120132, `asPascalCase` is not enough for all screen readers to understand that a word pair is not a single word.
    
    Going with a lot more direct approach with `"${pair.first} ${pair.second}”`.
    
    Fixes https://github.com/flutter/flutter/issues/120132.

--- namer/step_07_a_split_my_home_page/lib/main.dart
+++ namer/step_07_a_split_my_home_page/lib/main.dart
@@ -148,7 +148,7 @@ class BigCard extends StatelessWidget {
         child: Text(
           pair.asLowerCase,
           style: style,
-          semanticsLabel: pair.asPascalCase,
+          semanticsLabel: "${pair.first} ${pair.second}",
         ),
       ),
     );
