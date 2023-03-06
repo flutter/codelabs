eacc3ee74635ecba228ea401b8bfc82620f8d1ec
Brett Morgan <brettmorgan@google.com>
2023-03-06 17:53:43 +1000

    Get the smoke tests working

--- pesto_flutter/step_04/test/widget_test.dart
+++ pesto_flutter/step_04/test/widget_test.dart
@@ -1,9 +1,9 @@
-import 'package:pesto_example/main.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pesto_example/presentation/app.dart';
 
 void main() {
   testWidgets('Smoke test', (tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Pesto());
   });
 }
