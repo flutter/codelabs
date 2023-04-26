a97ee546e66793e869bd8296978dbba4c9723387
John Ryan <ryjohn@google.com>
2023-04-26 13:53:20 -0700

    Dart 3 patterns codelab step 01

--- dart3/step_01/lib/main.dart
+++ dart3/step_01/lib/main.dart
@@ -2,56 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const Dart3SampleApp());
+  runApp(const MainApp());
 }
 
-class Dart3SampleApp extends StatelessWidget {
-  const Dart3SampleApp({Key? key}) : super(key: key);
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  Map<String, dynamic>? json;
-
-  @override
-  void initState() {
-    parseJson();
-    super.initState();
-  }
-
-  Future<void> parseJson() async {
-    var jsonString = await rootBundle.loadString('assets/data.json');
-    var jsonMap = jsonDecode(jsonString);
-
-    setState(() {
-      json = jsonMap;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('$json'),
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
       ),
     );
   }
