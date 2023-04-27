3b144ab798f2752b95c396a46f04e426e7218393
Brett Morgan <brettmorgan@google.com>
2023-04-28 09:11:19 +1000

    Re-introduce `dart3` for I/O

---
+++ dart3/step_10/lib/main.dart
@@ -0,0 +1,86 @@
+// Copyright 2023 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+import 'data.dart';
+
+void main() {
+  runApp(const DocumentApp());
+}
+
+class DocumentApp extends StatelessWidget {
+  const DocumentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: DocumentScreen(
+        document: Document(),
+      ),
+    );
+  }
+}
+
+class DocumentScreen extends StatelessWidget {
+  final Document document;
+
+  const DocumentScreen({
+    required this.document,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var (title, :modified) = document.getMetadata();
+    var blocks = document.getBlocks();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Column(
+        children: [
+          Text('Last modified: $modified'),
+          Expanded(
+            child: ListView.builder(
+              itemCount: blocks.length,
+              itemBuilder: (context, index) {
+                return BlockWidget(block: blocks[index]);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+
+  const BlockWidget({
+    required this.block,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? textStyle;
+    textStyle = switch (block.type) {
+      'h1' => Theme.of(context).textTheme.displayMedium,
+      'p' || 'checkbox' => Theme.of(context).textTheme.bodyMedium,
+      _ => Theme.of(context).textTheme.bodySmall
+    };
+
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: Text(
+        block.text,
+        style: textStyle,
+      ),
+    );
+  }
+}
