2d5cb58df813a4b2a89e34234151a4a86ab9e0c0
Wayne Wei <windmaple@gmail.com>
2023-05-08 13:23:50 +0800

    Update haiku_generator to use generative AI API to write haikus (#1605)
    *Replace this paragraph with a description of what this PR is changing
    or adding, and why. Consider including before/after screenshots.*
    
    *List which issues are fixed by this PR. For larger changes, raising an
    issue first helps
    reduce redundant work.*
    
    ## Pre-launch Checklist
    
    - [ ] I read the [Effective Dart: Style] _recently_, and have followed
    its advice.
    - [ ] I signed the [CLA].
    - [ ] I updated/added relevant documentation (doc comments with `///`).
    - [ ] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-devrel
    channel on [Discord].
    
    <!-- Links -->
    [Effective Dart: Style]:
    https://dart.dev/guides/language/effective-dart/style
    [CLA]: https://cla.developers.google.com/
    [Discord]: https://github.com/flutter/flutter/wiki/Chat

---
+++ haiku_generator/step0/test/widget_test.dart
@@ -0,0 +1,21 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:haiku_generator/main.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that the widgets are there
+    expect(find.text('Choose a Google product here:'), findsOneWidget);
+    expect(find.text('Generate haiku!'), findsOneWidget);
+  });
+}
---
+++ haiku_generator/step3/lib/data/repositories/poem_repository_impl.dart
@@ -0,0 +1,54 @@
+import '../../domain/repositories/abstract/poem_repository.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class PoemRepositoryImpl implements PoemRepository {
+  PoemRepositoryImpl();
+
+  @override
+  Future<String> getPoems(String productName) async {
+    // TODO: Replace YOUR_API_KEY with your API key.
+    var apiKey = 'YOUR_API_KEY';
+    const haikuCount = 5;
+
+    final url = Uri.parse(
+        'https://generativelanguage.googleapis.com/v1beta2/models/chat-bison-001:generateMessage?key=$apiKey');
+    final headers = {'Content-Type': 'application/json'};
+    final body = jsonEncode({
+      "prompt": {
+        "context": "You are an awesome haiku writer.",
+        "examples": [
+          {
+            "input": {"content": "Write a haiku about Google Photos."},
+            "output": {
+              "content":
+                  "Google Photos, my friend\nA journey of a lifetime\nCaptured in pixels"
+            }
+          }
+        ],
+        "messages": [
+          {"content": "Write a cool, long haiku of for $productName"}
+        ]
+      },
+      "candidate_count": haikuCount,
+      "temperature": 1,
+    });
+
+    try {
+      final response = await http.post(url, headers: headers, body: body);
+      if (response.statusCode == 200) {
+        final decodedResponse = json.decode(response.body);
+        String haikus = 'Here are $haikuCount haikus about $productName:\n\n';
+        for (var i = 0; i < haikuCount; i++) {
+          haikus += '${i + 1}.\n';
+          haikus += decodedResponse['candidates'][i]['content'] + '\n\n';
+        }
+        return haikus;
+      } else {
+        return 'Request failed with status: ${response.statusCode}.\n\n${response.body}';
+      }
+    } catch (error) {
+      throw Exception('Error sending POST request: $error');
+    }
+  }
+}
