2462a59ac350e014d04af26047e1657ca9f5eeef
Miguel Beltran <m@beltran.work>
2023-05-24 16:49:42 +0200

    `boring_to_beautiful` codelab to Dart 3 patterns and registers (#1627)
    Adding Dart 3 Pattern and Registers examples to `boring_to_beautiful`
    codelab
    
    ## Pre-launch Checklist
    
    - [x] I read the [Effective Dart: Style] _recently_, and have followed
    its advice.
    - [x] I signed the [CLA].
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-devrel
    channel on [Discord].
    
    <!-- Links -->
    [Effective Dart: Style]:
    https://dart.dev/guides/language/effective-dart/style
    [CLA]: https://cla.developers.google.com/
    [Discord]: https://github.com/flutter/flutter/wiki/Chat
    
    ---------
    
    Co-authored-by: Brett Morgan <brett.morgan@gmail.com>

--- boring_to_beautiful/step_06/lib/src/shared/extensions.dart
+++ boring_to_beautiful/step_06/lib/src/shared/extensions.dart
@@ -64,25 +64,18 @@ extension BreakpointUtils on BoxConstraints {
 
 extension DurationString on String {
   /// Assumes a string (roughly) of the format '\d{1,2}:\d{2}'
-  Duration toDuration() {
-    final chunks = split(':');
-    if (chunks.length == 1) {
-      throw Exception('Invalid duration string: $this');
-    } else if (chunks.length == 2) {
-      return Duration(
-        minutes: int.parse(chunks[0].trim()),
-        seconds: int.parse(chunks[1].trim()),
-      );
-    } else if (chunks.length == 3) {
-      return Duration(
-        hours: int.parse(chunks[0].trim()),
-        minutes: int.parse(chunks[1].trim()),
-        seconds: int.parse(chunks[2].trim()),
-      );
-    } else {
-      throw Exception('Invalid duration string: $this');
-    }
-  }
+  Duration toDuration() => switch (split(':')) {
+        [var minutes, var seconds] => Duration(
+            minutes: int.parse(minutes.trim()),
+            seconds: int.parse(seconds.trim()),
+          ),
+        [var hours, var minutes, var seconds] => Duration(
+            hours: int.parse(hours.trim()),
+            minutes: int.parse(minutes.trim()),
+            seconds: int.parse(seconds.trim()),
+          ),
+        _ => throw Exception('Invalid duration string: $this'),
+      };
 }
 
 extension HumanizedDuration on Duration {
