8f462d906c21693ac9f6fae5aeacf769600a4a27
Brett Morgan <brettmorgan@google.com>
2023-06-16 16:48:08 +1000

    Add exported JSON files

--- tooling/claat_export/bin/claat_export.dart
+++ tooling/claat_export/bin/claat_export.dart
@@ -1,5 +1,69 @@
-import 'package:claat_export/claat_export.dart' as claat_export;
+import 'dart:convert';
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${claat_export.calculate()}!');
+import 'package:args/args.dart';
+import 'package:claat_export/client_secret.dart';
+import 'package:googleapis/docs/v1.dart' as google_docs;
+import 'package:googleapis_auth/auth_io.dart';
+
+void main(List<String> arguments) async {
+  final argParser = ArgParser();
+  argParser.addFlag(
+    'help',
+    abbr: 'h',
+    negatable: false,
+    help: 'Display usage',
+  );
+  argParser.addOption(
+    'client-secrets',
+    abbr: 's',
+    mandatory: true,
+    help: 'The path to the client_secrets.json file',
+  );
+  argParser.addOption(
+    'doc-id',
+    abbr: 'd',
+    mandatory: true,
+    help: 'The document ID to export',
+  );
+  argParser.parse(arguments);
+  final args = argParser.parse(arguments);
+  if (args['help']) {
+    print(argParser.usage);
+    exit(-1);
+  }
+
+  final clientSecret = ClientSecret.fromJson(
+    jsonDecode(
+      await File(args['client-secrets']).readAsString(),
+    ),
+  );
+
+  final gDocID = args['doc-id'];
+
+  final client = await obtainCredentials(
+    clientID: clientSecret.installed.clientId,
+    clientSecret: clientSecret.installed.clientSecret,
+  );
+  final apiClient = google_docs.DocsApi(client);
+  final document = await apiClient.documents
+      .get(gDocID, suggestionsViewMode: 'PREVIEW_WITHOUT_SUGGESTIONS');
+  final enc = JsonEncoder.withIndent('  ');
+  final output = File('$gDocID.json');
+  output.writeAsString(enc.convert(document.toJson()));
+  print('Wrote to ${output.path}');
+}
+
+Future<AuthClient> obtainCredentials(
+        {required String clientID, required String clientSecret}) async =>
+    await clientViaUserConsent(
+      ClientId(clientID, clientSecret),
+      [google_docs.DocsApi.driveReadonlyScope],
+      _prompt,
+    );
+
+void _prompt(String url) {
+  print('Please go to the following URL and grant access:');
+  print('  => $url');
+  print('');
 }
--- tooling/claat_export/test/data/README.md
+++ tooling/claat_export/test/data/README.md
@@ -9,4 +9,11 @@ ok      codelabs/flutter-cupertino
 
 Note, the exported image files are not checked in.
 
+The files under `exports` are created as follows:
+
+```console
+$ cd exports
+$ dart run ../../../bin/claat_export.dart -s ../../../client_secret.apps.googleusercontent.com.json -d 1MfCrv1w6aK7SLq9gmjJwXCJFQ-AX9NWJit2gq-v2Xx8
+```
+
   [`claat`]: https://github.com/googlecodelabs/tools/tree/main/claat
