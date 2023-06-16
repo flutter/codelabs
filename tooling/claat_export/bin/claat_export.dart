c7991bda7223dd58c630948b0cbe95c628985653
Brett Morgan <brettmorgan@google.com>
2023-06-16 13:57:37 +1000

    `dart create -t console claat_export`

---
+++ tooling/claat_export/bin/claat_export.dart
@@ -0,0 +1,5 @@
+import 'package:claat_export/claat_export.dart' as claat_export;
+
+void main(List<String> arguments) {
+  print('Hello world: ${claat_export.calculate()}!');
+}
---
+++ tooling/claat_export/test/claat_export_test.dart
@@ -0,0 +1,8 @@
+import 'package:claat_export/claat_export.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
