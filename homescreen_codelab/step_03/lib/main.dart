ea5a8e00895c1faed76d30528169c5c76cd9c3f4
Leigha Jarett <leigha.jarett@gmail.com>
2023-06-27 19:35:10 -0400

    Adding homescreen widgets codelab (#1728)
    This adds the homescreen widget codelab.
    
    I have tested the iOS start and final, but I couldn't get the Android
    start or final to work because of some gradle issue. @ericwindmill can
    you verify that this is all working on Android?
    
    Outside of @domesticmouse's review, here are the remaining TO DOs for
    merging this PR:
    
    - [x] @ericwindmill to test current version start and final on Android
    - [x] Break into 4 steps to match the written codelab (after Brett's
    review)
    
    ## Pre-launch Checklist
    
    - [X] I read the [Effective Dart: Style] _recently_, and have followed
    its advice.
    - [X] I signed the [CLA].
    - [X] I updated/added relevant documentation (doc comments with `///`).
    - [X] All existing and new tests are passing.
    
    ---------
    
    Co-authored-by: Eric Windmill <ewindmill@google.com>

---
+++ homescreen_codelab/step_03/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        appBarTheme: AppBarTheme(
+          backgroundColor: ColorScheme.fromSeed(seedColor: Colors.deepPurple)
+              .primaryContainer,
+        ),
+        textTheme: const TextTheme(
+          titleMedium: TextStyle(
+            fontFamily: 'Chewy',
+            fontSize: 20,
+          ),
+        ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
---
+++ homescreen_codelab/step_04/ios/NewsWidgets/NewsWidgetsBundle.swift
@@ -0,0 +1,9 @@
+import WidgetKit
+import SwiftUI
+
+@main
+struct NewsWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        NewsWidgets()
+    }
+}
