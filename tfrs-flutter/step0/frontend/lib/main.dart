243ae7d6cb710a666667c0d3da845beb94a157d2
Miguel Beltran <m@beltran.work>
2023-07-18 16:20:33 +0200

    Use FilledButton

--- tfrs-flutter/step0/frontend/lib/main.dart
+++ tfrs-flutter/step0/frontend/lib/main.dart
@@ -67,8 +67,8 @@ class _RecommenderDemoState extends State<RecommenderDemo> {
                         )),
                         Container(
                             margin: const EdgeInsets.only(left: 10.0),
-                            child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
+                            child: FilledButton(
+                                style: FilledButton.styleFrom(
                                   textStyle: const TextStyle(fontSize: 15),
                                 ),
                                 onPressed: () {
