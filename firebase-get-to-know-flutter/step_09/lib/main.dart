fa9f7c6f4219fc03982a7461e357f4c8653738a6
droidbg <binni1108@gmail.com>
2023-11-05 17:40:26 +0530

    Refactor. primarySwatch

--- firebase-get-to-know-flutter/step_09/lib/main.dart
+++ firebase-get-to-know-flutter/step_09/lib/main.dart
@@ -121,7 +121,7 @@ class App extends StatelessWidget {
         buttonTheme: Theme.of(context).buttonTheme.copyWith(
               highlightColor: Colors.deepPurple,
             ),
-        primarySwatch: Colors.deepPurple,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         textTheme: GoogleFonts.robotoTextTheme(
           Theme.of(context).textTheme,
         ),
