aceb0180c3706f43cd733c90bcf7775f38fa4808
Brett Morgan <brettmorgan@google.com>
2023-12-20 15:40:58 +1100

    Update rebuild scripts

--- firebase-auth-flutterfire-ui/start/macos/Flutter/GeneratedPluginRegistrant.swift
+++ firebase-auth-flutterfire-ui/start/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import desktop_webview_auth
 import firebase_auth
 import firebase_core
+import google_sign_in_ios
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
 }
