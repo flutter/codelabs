40e280c678a0bef3b92262d92ab2dd617e5bbf9a
Brett Morgan <brettmorgan@google.com>
2024-02-02 10:55:16 +1100

    Update `animated-responsive-layout`

--- animated-responsive-layout/step_03/android/app/src/main/kotlin/com/example/animated_responsive_layout/MainActivity.kt
+++ animated-responsive-layout/step_03/android/app/src/main/kotlin/com/example/animated_responsive_layout/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.animated_responsive_layout
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
