78f4313869b6972436d72542e9219c456a6505de
Brett Morgan <brettmorgan@google.com>
2024-03-26 20:45:46 +1100

    step 3

---
+++ generate_crossword/step_03/lib/main.dart
@@ -0,0 +1,30 @@
+// Copyright 2024 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(
+    ProviderScope(
+      child: MaterialApp(
+        title: 'Crossword Builder',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          colorSchemeSeed: Colors.blueGrey,
+          brightness: Brightness.light,
+        ),
+        home: Scaffold(
+          body: Center(
+            child: Text(
+              'Hello, World!',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
