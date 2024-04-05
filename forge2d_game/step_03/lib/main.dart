870a14c96caba5e4ce96b5c99089c1a928e7d6a0
Brett Morgan <brettmorgan@google.com>
2024-04-05 12:02:50 +1100

    Step 3

---
+++ forge2d_game/step_03/lib/main.dart
@@ -0,0 +1,10 @@
+// Copyright 2024 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GameWidget.controlled(gameFactory: FlameGame.new));
+}
