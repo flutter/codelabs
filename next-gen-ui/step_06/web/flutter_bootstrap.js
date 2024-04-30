e8a208dbb326d61e9165cb8270b8344e8c672395
Brett Morgan <brett.morgan@gmail.com>
2024-04-30 11:02:07 +1000

    Simplify bootstrap

--- next-gen-ui/step_06/web/flutter_bootstrap.js
+++ next-gen-ui/step_06/web/flutter_bootstrap.js
@@ -2,8 +2,7 @@
 {{flutter_build_config}}
 
 _flutter.loader.load({
-  config: {'renderer': 'canvaskit'},
-  serviceWorkerSettings: {
-    serviceWorkerVersion: {{flutter_service_worker_version}},
+  config: {
+    renderer: 'canvaskit',
   },
 });
