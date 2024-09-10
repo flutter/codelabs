07806ed8d7f1629ed915b53a2682d2506bc53c24
Brett Morgan <brettmorgan@google.com>
2024-09-10 17:59:49 +1000

    `animated-responsive-layout`

--- animated-responsive-layout/step_06/lib/main.dart
+++ animated-responsive-layout/step_06/lib/main.dart
@@ -41,7 +41,7 @@ class Feed extends StatefulWidget {
 class _FeedState extends State<Feed> {
   late final _colorScheme = Theme.of(context).colorScheme;
   late final _backgroundColor = Color.alphaBlend(
-      _colorScheme.primary.withOpacity(0.14), _colorScheme.surface);
+      _colorScheme.primary.withAlpha(36), _colorScheme.surface);
 
   int selectedIndex = 0;
   bool wideScreen = false;
