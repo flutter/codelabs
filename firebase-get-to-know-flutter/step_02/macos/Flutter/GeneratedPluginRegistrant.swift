58089cd7d252d3c1230551247198d7683ac103f5
LisbethMendoza <lisbethango223019@gmail.com>
2024-11-19 14:51:03 -0400

    Feat: Firts commit

--- firebase-get-to-know-flutter/step_02/macos/Flutter/GeneratedPluginRegistrant.swift
+++ firebase-get-to-know-flutter/step_02/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
+import desktop_webview_auth
+import firebase_auth
+import firebase_core
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
