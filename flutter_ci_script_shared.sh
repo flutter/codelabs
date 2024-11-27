a5e77c9ae714497c0f9c467feb545cf5fda2154d
Brett Morgan <brett.morgan@gmail.com>
2024-11-28 09:12:50 +1100

    Simplify

--- flutter_ci_script_shared.sh
+++ flutter_ci_script_shared.sh
@@ -5,21 +5,6 @@ function ci_codelabs () {
     # Disable analytics to avoid https://github.com/dart-lang/tools/issues/249
     dart --disable-analytics
 
-    # intro_flutter_gpu requires the master channel and macOS
-    if [ $channel = "master" && $RUNNER_OS = 'macOS' ]; then
-        # Enable native assets for intro_flutter_gpu
-        flutter config --enable-native-assets
-
-        # intro_flutter_gpu needs to build the shaders before running the tests
-        for step in "01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12" 
-        do
-            pushd intro_flutter_gpu/step_$step
-                flutter build `echo $RUNNER_OS | tr '[:upper:]' '[:lower:]'` --debug
-            popd
-        done    
-    fi
-
-
     # ffigen_codelab/step_07 needs to build the native library before running the tests
     pushd ffigen_codelab/step_07/example
         # RUNNER_OS from https://stackoverflow.com/a/72926104/2142626
@@ -66,6 +51,8 @@ function ci_codelabs () {
                     # intro_flutter_gpu only runs with Impeller on macOS
                     if [ $CODELAB = 'intro_flutter_gpu' ]; then
                         if [ $RUNNER_OS = 'macOS' ]; then
+                            flutter config --enable-native-assets
+                            flutter build `echo $RUNNER_OS | tr '[:upper:]' '[:lower:]'` --debug
                             flutter test --enable-impeller                    
                         fi
                         # This skips the test if the runner OS is not macOS
