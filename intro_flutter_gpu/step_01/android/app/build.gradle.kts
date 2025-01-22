1f4835bd613d7584220d855d240e0992ce4633ea
Brett Morgan <brett.morgan@gmail.com>
2025-01-22 15:04:43 +1100

    Update `intro_flutter_gpu`

--- intro_flutter_gpu/step_01/android/app/build.gradle.kts
+++ intro_flutter_gpu/step_01/android/app/build.gradle.kts
@@ -11,12 +11,12 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 
     defaultConfig {
