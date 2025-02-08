a6d1cb08c39a497b8ea6aa1cc966e5f44aaa2107
Brett Morgan <brett.morgan@gmail.com>
2025-02-08 11:41:52 +1100

    Update `namer`

--- namer/step_05_d_theme/lib/main.dart
+++ namer/step_05_d_theme/lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Namer App',
         theme: ThemeData(
-          
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
         home: MyHomePage(),
