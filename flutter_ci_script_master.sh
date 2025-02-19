6ac07590f7fb3518bae32f45dbe7e83627411a1d
Brett Morgan <brett.morgan@gmail.com>
2025-02-19 12:32:39 +1100

    Fixup CI

--- flutter_ci_script_master.sh
+++ flutter_ci_script_master.sh
@@ -6,7 +6,8 @@ DIR="${BASH_SOURCE%/*}"
 source "$DIR/flutter_ci_script_shared.sh"
 
 declare -a CODELABS=(
-  "adaptive_app"
+  # TODO(DomesticMouse): The argument type 'TabBarTheme' can't be assigned to the parameter type 'TabBarThemeData?'.
+  # "adaptive_app"
   "animated-responsive-layout"
   "animations"
   "audio_soloud"
