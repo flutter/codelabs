801690436c2e11ed8d9f8c16c600f2c02df666e6
Brett Morgan <brett.morgan@gmail.com>
2025-02-26 11:51:46 +1100

    Add rebuild script for `haiku_generator` (#2291)

--- haiku_generator/step2/lib/data/repositories/poem_repository_impl.dart
+++ haiku_generator/step2/lib/data/repositories/poem_repository_impl.dart
@@ -1,14 +1,16 @@
-import '../../domain/repositories/abstract/poem_repository.dart';
 // ignore: unused_import
 import 'dart:convert';
+
 // ignore: unused_import
 import 'package:http/http.dart' as http;
+
+import '../../domain/repositories/abstract/poem_repository.dart';
 
 class PoemRepositoryImpl implements PoemRepository {
   PoemRepositoryImpl();
 
   @override
   Future<String> getPoems(String productName) async {
-    return "";
+    return '';
   }
 }
