d055431e64e1859a5280b080c7a2a86ee9456193
Robert Odrowaz <robert@odrowaz.dev>
2025-03-13 18:44:58 +0100

    `in_app_purchases` codelab update (#2295)
    This PR is for a matching docs update. There are only minor changes in
    the code:
    1. `googlePlayProjectName` -> `googleCloudProjectId` const rename to
    make it more appropriate. It is used for Pub/Sub to which Google Play
    publishes purchase-related events but it's on Google Cloud so we need
    the ID of the GCP project.
    2. Steps `09` and `10` were misaligned relative to the instructions and
    should be steps `10` and `11` instead.
    3. Minor update to backend readme. The `constants.dart` already exists,
    there was a typo in one of the placeholder values, and `constants.dart`
    uses single quotes.
    
    ## Pre-launch Checklist
    
    - [x] I read the [Effective Dart: Style] _recently_, and have followed
    its advice.
    - [x] I signed the [CLA].
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-devrel
    channel on [Discord].
    
    <!-- Links -->
    [Effective Dart: Style]:
    https://dart.dev/guides/language/effective-dart/style
    [CLA]: https://cla.developers.google.com/
    [Discord]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Chat.md

--- in_app_purchases/complete/dart-backend/lib/constants.dart
+++ in_app_purchases/complete/dart-backend/lib/constants.dart
@@ -3,5 +3,5 @@ const appStoreIssuerId = 'App Store Key issuer ID';
 const appStoreKeyId = 'App Store Key ID';
 const appStoreSharedSecret = 'App Store shared secret';
 const bundleId = 'your iOS bundle ID';
-const googlePlayProjectName = 'Google Cloud project name';
+const googleCloudProjectId = 'Google Cloud project ID';
 const googlePlayPubsubBillingTopic = 'play_billing';
--- in_app_purchases/step_10/dart-backend/bin/server.dart
+++ in_app_purchases/step_10/dart-backend/bin/server.dart
@@ -5,9 +5,7 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:app_store_server_sdk/app_store_server_sdk.dart';
 import 'package:firebase_backend_dart/app_store_purchase_handler.dart';
-import 'package:firebase_backend_dart/constants.dart';
 import 'package:firebase_backend_dart/google_play_purchase_handler.dart';
 import 'package:firebase_backend_dart/helpers.dart';
 import 'package:firebase_backend_dart/iap_repository.dart';
@@ -15,7 +13,6 @@ import 'package:firebase_backend_dart/products.dart';
 import 'package:firebase_backend_dart/purchase_handler.dart';
 import 'package:googleapis/androidpublisher/v3.dart' as ap;
 import 'package:googleapis/firestore/v1.dart' as fs;
-import 'package:googleapis/pubsub/v1.dart' as pubsub;
 import 'package:googleapis_auth/auth_io.dart' as auth;
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
@@ -29,15 +26,11 @@ Future<Map<String, PurchaseHandler>> _createPurchaseHandlers() async {
   final clientCredentialsGooglePlay = auth.ServiceAccountCredentials.fromJson(
     serviceAccountGooglePlay,
   );
-  final clientGooglePlay = await auth
-      .clientViaServiceAccount(clientCredentialsGooglePlay, [
-        ap.AndroidPublisherApi.androidpublisherScope,
-        pubsub.PubsubApi.cloudPlatformScope,
-      ]);
+  final clientGooglePlay = await auth.clientViaServiceAccount(
+    clientCredentialsGooglePlay,
+    [ap.AndroidPublisherApi.androidpublisherScope],
+  );
   final androidPublisher = ap.AndroidPublisherApi(clientGooglePlay);
-
-  // Pub/Sub API to receive on purchase events from Google Play
-  final pubsubApi = pubsub.PubsubApi(clientGooglePlay);
 
   // Configure Firestore API access
   final serviceAccountFirebase =
@@ -54,41 +47,9 @@ Future<Map<String, PurchaseHandler>> _createPurchaseHandlers() async {
   final projectId = json['project_id'] as String;
   final iapRepository = IapRepository(firestoreApi, projectId);
 
-  final subscriptionKeyAppStore =
-      File('assets/SubscriptionKey.p8').readAsStringSync();
-
-  // Configure Apple Store API access
-  var appStoreEnvironment = AppStoreEnvironment.sandbox(
-    bundleId: bundleId,
-    issuerId: appStoreIssuerId,
-    keyId: appStoreKeyId,
-    privateKey: subscriptionKeyAppStore,
-  );
-
-  // Stored token for Apple Store API access, if available
-  final file = File('assets/appstore.token');
-  String? appStoreToken;
-  if (file.existsSync() && file.lengthSync() > 0) {
-    appStoreToken = file.readAsStringSync();
-  }
-
-  final appStoreServerAPI = AppStoreServerAPI(
-    AppStoreServerHttpClient(
-      appStoreEnvironment,
-      jwt: appStoreToken,
-      jwtTokenUpdatedCallback: (token) {
-        file.writeAsStringSync(token);
-      },
-    ),
-  );
-
   return {
-    'google_play': GooglePlayPurchaseHandler(
-      androidPublisher,
-      iapRepository,
-      pubsubApi,
-    ),
-    'app_store': AppStorePurchaseHandler(iapRepository, appStoreServerAPI),
+    'google_play': GooglePlayPurchaseHandler(androidPublisher, iapRepository),
+    'app_store': AppStorePurchaseHandler(iapRepository),
   };
 }
 
