c4dd1d23261c0a154a81d39c57fd520e44e08c88
Brett Morgan <brett.morgan@gmail.com>
2025-03-29 04:57:04 +1100

    ndkVersion fix, steps 5 to 7 initial boilerplate

---
+++ colorist/step_05/lib/services/gemini_chat_service.dart
@@ -0,0 +1,42 @@
+// Copyright 2025 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:colorist_ui/models/message.dart';
+import 'package:colorist_ui/providers/chat_state_notifier.dart';
+import 'package:colorist_ui/providers/log_state_notifier.dart';
+import 'package:firebase_vertexai/firebase_vertexai.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../providers/gemini.dart';
+
+part 'gemini_chat_service.g.dart';
+
+class GeminiChatService {
+  GeminiChatService(this.ref);
+  final Ref ref;
+
+  Future<void> sendMessage(String message) async {
+    final chatSession = await ref.read(chatSessionProvider.future);
+    final chatStateNotifier = ref.read(chatStateNotifierProvider.notifier);
+    final logStateNotifier = ref.read(logStateNotifierProvider.notifier);
+
+    chatStateNotifier.addUserMessage(message);
+    logStateNotifier.logUserText(message);
+    try {
+      final responseText =
+          (await chatSession.sendMessage(Content.text(message))).text?.trim() ??
+          'No text response received';
+      logStateNotifier.logLlmText(responseText);
+      chatStateNotifier.addLlmMessage(responseText, MessageState.complete);
+    } catch (e, st) {
+      logStateNotifier.logError(e, st: st);
+    }
+  }
+}
+
+@riverpod
+GeminiChatService geminiChatService(Ref ref) => GeminiChatService(ref);
