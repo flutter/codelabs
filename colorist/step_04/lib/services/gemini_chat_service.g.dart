6582e57b0869b3d14fadfa4e0cffcac6537d4f92
Brett Morgan <brett.morgan@gmail.com>
2025-03-28 17:23:13 +1100

    Step 4 initial, plus fixups

---
+++ colorist/step_04/lib/services/gemini_chat_service.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'gemini_chat_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$geminiChatServiceHash() => r'62a58e95c6112ec2cc1b4777a88270e75fe2949f';
+
+/// See also [geminiChatService].
+@ProviderFor(geminiChatService)
+final geminiChatServiceProvider =
+    AutoDisposeProvider<GeminiChatService>.internal(
+      geminiChatService,
+      name: r'geminiChatServiceProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$geminiChatServiceHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef GeminiChatServiceRef = AutoDisposeProviderRef<GeminiChatService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
