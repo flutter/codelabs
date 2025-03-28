63b5ffed84e5f2684975e7fe44035ed797c8df4b
Brett Morgan <brett.morgan@gmail.com>
2025-03-28 17:15:30 +1100

    Fixup

--- colorist/step_03/lib/providers/system_prompt.dart
+++ colorist/step_03/lib/providers/system_prompt.dart
@@ -10,4 +10,4 @@ part 'system_prompt.g.dart';
 
 @riverpod
 Future<String> systemPrompt(Ref ref) =>
-    rootBundle.loadString('assets/prompts/system_prompt_with_tools.md');
+    rootBundle.loadString('assets/prompts/system_prompt.md');
