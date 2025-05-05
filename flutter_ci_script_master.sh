de50a8270d7f7b95e07511ce89c8563d5cefe3fd
Brett Morgan <brett.morgan@gmail.com>
2025-05-06 07:23:50 +1000

    Update flutter_ci_script_master.sh

--- flutter_ci_script_master.sh
+++ flutter_ci_script_master.sh
@@ -6,7 +6,8 @@ DIR="${BASH_SOURCE%/*}"
 source "$DIR/flutter_ci_script_shared.sh"
 
 declare -a CODELABS=(
-  "adaptive_app"
+  # TODO(DomesticMouse): Dart format
+  # "adaptive_app"
   "animated-responsive-layout"
   "animations"
   "audio_soloud"
