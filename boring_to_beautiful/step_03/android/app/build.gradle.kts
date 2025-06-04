a2adce1e57141814fab954ca50ecef3c4d99a9a0
Brett Morgan <brett.morgan@gmail.com>
2025-06-04 15:03:01 +1000

    Set `nkdVersion`

--- boring_to_beautiful/step_03/android/app/build.gradle.kts
+++ boring_to_beautiful/step_03/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.myartist"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
