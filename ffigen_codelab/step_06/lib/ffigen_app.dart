b028f8f4dac691e37679c7f42338f93c4ecdd722
Brett Morgan <brett.morgan@gmail.com>
2025-06-18 16:30:40 +1000

    Clean up `ffigen_app.dart`

--- ffigen_codelab/step_06/lib/ffigen_app.dart
+++ ffigen_codelab/step_06/lib/ffigen_app.dart
@@ -10,7 +10,6 @@ import 'duktape_bindings_generated.dart';
 
 const String _libName = 'ffigen_app';
 
-/// The dynamic library in which the symbols for [DuktapeBindings] can be found.
 final DynamicLibrary _dylib = () {
   if (Platform.isMacOS || Platform.isIOS) {
     return DynamicLibrary.open('$_libName.framework/$_libName');
@@ -24,7 +23,6 @@ final DynamicLibrary _dylib = () {
   throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
 }();
 
-/// The bindings to the native functions in [_dylib].
 final DuktapeBindings _bindings = DuktapeBindings(_dylib);
 
 class Duktape {
@@ -39,16 +37,12 @@ class Duktape {
   }
 
   void evalString(String jsCode) {
-    // From duktape.h:
-    // #define duk_peval_string(ctx,src)  \
-    // 	(duk_eval_raw((ctx), (src), 0, 0 /*args*/ | DUK_COMPILE_EVAL | DUK_COMPILE_SAFE | DUK_COMPILE_NOSOURCE | DUK_COMPILE_STRLEN | DUK_COMPILE_NOFILENAME))
-
     var nativeUtf8 = jsCode.toNativeUtf8();
     _bindings.duk_eval_raw(
       ctx,
       nativeUtf8.cast<Char>(),
       0,
-      0 /*args*/ |
+      0 |
           DUK_COMPILE_EVAL |
           DUK_COMPILE_SAFE |
           DUK_COMPILE_NOSOURCE |
