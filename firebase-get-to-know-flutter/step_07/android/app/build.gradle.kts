1dc81dcf81a7be044e84fc01607f47ab330b8775
Brett Morgan <brett.morgan@gmail.com>
2025-08-01 09:38:30 +1000

    Update `firebase-get-to-know-flutter`

--- firebase-get-to-know-flutter/step_07/android/app/build.gradle.kts
+++ firebase-get-to-know-flutter/step_07/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.gtk_flutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
