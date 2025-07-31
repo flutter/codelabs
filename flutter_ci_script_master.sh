c888d9d8c1c644afefe6a5e1978427af0a013346
Brett Morgan <brett.morgan@gmail.com>
2025-07-31 17:27:11 +1000

    Elide `firebase-get-to-know-flutter` (#2335)

--- flutter_ci_script_master.sh
+++ flutter_ci_script_master.sh
@@ -20,7 +20,8 @@ declare -a CODELABS=(
   "ffigen_codelab"
   "firebase-auth-flutterfire-ui"
   "firebase-emulator-suite"
-  "firebase-get-to-know-flutter"
+  # TODO(DomesticMouse): Could not load AssetManifest.json
+  # "firebase-get-to-know-flutter"
   "forge2d_game"
   "generate_crossword"
   "github-client"
