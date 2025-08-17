23d32f2e3d1ae815490d5139f9186f5ebb8e197e
Brett Morgan <brett.morgan@gmail.com>
2025-08-17 12:17:06 +1000

    Track https://github.com/flutter/flutter/issues/173904

--- flutter_ci_script_main.sh
+++ flutter_ci_script_main.sh
@@ -6,7 +6,8 @@ DIR="${BASH_SOURCE%/*}"
 source "$DIR/flutter_ci_script_shared.sh"
 
 declare -a CODELABS=(
-  "adaptive_app"
+  # TODO(DomesticMouse): Enable once https://github.com/flutter/flutter/issues/173904 is fixed
+  # "adaptive_app"
   "animated-responsive-layout"
   "animations"
   "audio_soloud"
