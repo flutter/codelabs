eac8de6f1987928e4493d92f110dafb7d0280e54
vive0526 <vivethanstylo@gmail.com>
2025-10-29 08:51:55 +0800

    Update app.dart

--- boring_to_beautiful/step_03/lib/src/shared/app.dart
+++ boring_to_beautiful/step_03/lib/src/shared/app.dart
@@ -1,7 +1,3 @@
-// Copyright 2022 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,55 +5,48 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'playback/bloc/bloc.dart';
 import 'providers/theme.dart';
 import 'router.dart';
-import 'views/views.dart';
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+ const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
+ @override
+ State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  final settings = ValueNotifier(
-    ThemeSettings(sourceColor: Colors.pink, themeMode: ThemeMode.system),
-  );
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider<PlaybackBloc>(
-      create: (context) => PlaybackBloc(),
-      child: DynamicColorBuilder(
-        builder: (lightDynamic, darkDynamic) => ThemeProvider(
-          lightDynamic: lightDynamic,
-          darkDynamic: darkDynamic,
-          settings: settings,
-          child: NotificationListener<ThemeSettingChange>(
-            onNotification: (notification) {
-              settings.value = notification.settings;
-              return true;
-            },
-            child: ValueListenableBuilder<ThemeSettings>(
-              valueListenable: settings,
-              builder: (context, value, _) {
-                final theme = ThemeProvider.of(context);
-                return MaterialApp.router(
-                  debugShowCheckedModeBanner: false,
-                  title: 'Flutter Demo',
-                  theme: theme.light(settings.value.sourceColor),
-                  darkTheme: theme.dark(settings.value.sourceColor),
-                  themeMode: theme.themeMode(),
-                  routeInformationParser: appRouter.routeInformationParser,
-                  routeInformationProvider: appRouter.routeInformationProvider,
-                  routerDelegate: appRouter.routerDelegate,
-                  builder: (context, child) {
-                    return PlayPauseListener(child: child!);
-                  },
-                );
-              },
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+ final settings = ValueNotifier(ThemeSettings(
+   sourceColor:  Colors.pink,
+   themeMode: ThemeMode.system,
+ ));
+ @override
+ Widget build(BuildContext context) {
+   return BlocProvider<PlaybackBloc>(
+     create: (context) => PlaybackBloc(),
+     child: DynamicColorBuilder(
+       builder: (lightDynamic, darkDynamic) => ThemeProvider(
+           lightDynamic: lightDynamic,
+           darkDynamic: darkDynamic,
+           settings: settings,
+           child: NotificationListener<ThemeSettingChange>(
+             onNotification: (notification) {
+               settings.value = notification.settings;
+               return true;
+             },
+             child: ValueListenableBuilder<ThemeSettings>(
+               valueListenable: settings,
+               builder: (context, value, _) {
+                 final theme = ThemeProvider.of(context); // Add this line
+                 return MaterialApp.router(
+                   debugShowCheckedModeBanner: false,
+                   title: 'Flutter Demo',
+                   theme: theme.light(settings.value.sourceColor), // Add this line
+                   routeInformationParser: appRouter.routeInformationParser,
+                   routerDelegate: appRouter.routerDelegate,
+                 );
+               },
+             ),
+           )),
+     ),
+   );
+ }
 }
